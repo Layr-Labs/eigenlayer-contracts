3d2668f8e14829d8fbda7325c86b84cf9ae97083
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-05-15 15:18:11 -0700

    additional top-level documentation for Pausable.sol

--- src/contracts/permissions/Pausable.sol
+++ src/contracts/permissions/Pausable.sol
@@ -7,10 +7,17 @@ import "../interfaces/IPausable.sol";
 /**
  * @title Adds pausability to a contract.
  * @author Layr Labs, Inc.
- * @notice Contracts that inherit from this contract define their own `pause` and `unpause` (and/or related) functions.
+ * @notice Contracts that inherit from this contract may define their own `pause` and `unpause` (and/or related) functions.
  * These functions should be permissioned as "onlyPauser" which defers to a `PauserRegistry` for determining access control.
- * @dev Pausability is implemented using a uint256, which allows up to 256 different bit-flags; each bit can potentially pause different functionality.
- * Inspiration is taken from the NearBridge design here https://etherscan.io/address/0x3FEFc5A4B1c02f21cBc8D3613643ba0635b9a873#code
+ * @dev Pausability is implemented using a uint256, which allows up to 256 different single bit-flags; each bit can potentially pause different functionality.
+ * Inspiration for this was taken from the NearBridge design here https://etherscan.io/address/0x3FEFc5A4B1c02f21cBc8D3613643ba0635b9a873#code.
+ * For the `pause` and `unpause` functions we've implemented, if you pause, you can only flip (any number of) switches to on/1 (aka "paused"), and if you unpause,
+ * you can only flip (any number of) switches to off/0 (aka "paused").
+ * If you want a pauseXYZ function that just flips a single bit / "pausing flag", it will:
+ * 1) 'bit-wise and' (aka `&`) a flag with the current paused state (as a uint256)
+ * 2) update the paused state to this new value
+ * @dev We note as well that we have chosen to identify flags by their *bit index* as opposed to their numerical value, so, e.g. defining `DEPOSITS_PAUSED = 3`
+ * indicates specifically that if the *third bit* of `_paused` is flipped -- i.e. it is a '1' -- then deposits should be paused
  */
 contract Pausable is IPausable {
     /// @notice Address of the `PauserRegistry` contract that this contract defers to for determining access control (for pausing).
