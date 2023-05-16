6fbcabad13c0b02852f7670f65320be51c3abe24
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-05-16 13:47:07 -0700

    use correct gap size
    gap size should be 48 instead of 50, because we use 2 slots for the `maxPerDeposit` and `maxTotalDeposits` variables

--- src/contracts/strategies/StrategyBaseTVLLimits.sol
+++ src/contracts/strategies/StrategyBaseTVLLimits.sol
@@ -71,5 +71,5 @@ contract StrategyBaseTVLLimits is StrategyBase {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[50] private __gap;
+    uint256[48] private __gap;
 }
