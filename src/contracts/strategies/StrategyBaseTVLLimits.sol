ae43e62ac6a8bcab229285c586fcefef5eb2e239
gpsanant <gpsanant@gmail.com>
2023-06-07 09:19:26 -0700

    changed permissions to unpauser changing tvl limits

--- src/contracts/strategies/StrategyBaseTVLLimits.sol
+++ src/contracts/strategies/StrategyBaseTVLLimits.sol
@@ -35,11 +35,11 @@ contract StrategyBaseTVLLimits is StrategyBase {
     /**
      * @notice Sets the maximum deposits (in underlyingToken) that this strategy will hold and accept per deposit
      * @param newMaxTotalDeposits The new maximum deposits
-     * @dev Callable only by the pauser of this contract
+     * @dev Callable only by the unpauser of this contract
      * @dev We note that there is a potential race condition between a call to this function that lowers either or both of these limits and call(s)
      * to `deposit`, that may result in some calls to `deposit` reverting.
      */
-    function setTVLLimits(uint256 newMaxPerDeposit, uint256 newMaxTotalDeposits) external onlyPauser {
+    function setTVLLimits(uint256 newMaxPerDeposit, uint256 newMaxTotalDeposits) external onlyUnpauser {
         _setTVLLimits(newMaxPerDeposit, newMaxTotalDeposits);
     }
 
