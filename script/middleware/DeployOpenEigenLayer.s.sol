a64db108cc6683e28fcefee8d86c8c74057aec13
gpsanant <gpsanant@gmail.com>
2023-06-27 14:15:28 -0700

    add open eigenlayer deploy script

---
+++ script/middleware/DeployOpenEigenLayer.s.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+
+import "../../src/contracts/interfaces/IETHPOSDeposit.sol";
+import "../../src/contracts/interfaces/IBeaconChainOracle.sol";
+
+import "../../src/contracts/core/StrategyManager.sol";
+import "../../src/contracts/core/Slasher.sol";
+import "../../src/contracts/core/DelegationManager.sol";
+
+import "../../src/contracts/strategies/StrategyBaseTVLLimits.sol";
+
+import "../../src/contracts/pods/EigenPod.sol";
+import "../../src/contracts/pods/EigenPodManager.sol";
+import "../../src/contracts/pods/DelayedWithdrawalRouter.sol";
+
+import "../../src/contracts/permissions/PauserRegistry.sol";
+import "../../src/contracts/middleware/BLSPublicKeyCompendium.sol";
+
+import "../../src/test/mocks/EmptyContract.sol";
+import "../../src/test/mocks/ETHDepositMock.sol";
+
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+
+// # To load the variables in the .env file
+// source .env
+
+// # To deploy and verify our contract
+// forge script script/M1_Deploy.s.sol:Deployer_M1 --rpc-url $RPC_URL  --private-key $PRIVATE_KEY --broadcast -vvvv
+contract DeployOpenEigenLayer is Script, Test {
+    Vm cheats = Vm(HEVM_ADDRESS);
+
+    // struct used to encode token info in config file
+    struct StrategyConfig {
+        uint256 maxDeposits;
+        uint256 maxPerDeposit;
+        address tokenAddress;
+        string tokenSymbol;
+    }
+
+    // EigenLayer Contracts
+    ProxyAdmin public eigenLayerProxyAdmin;
+    PauserRegistry public eigenLayerPauserReg;
+    Slasher public slasher;
+    Slasher public slasherImplementation;
+    DelegationManager public delegation;
+    DelegationManager public delegationImplementation;
+    StrategyManager public strategyManager;
+    StrategyManager public strategyManagerImplementation;
+    EigenPodManager public eigenPodManager;
+    EigenPodManager public eigenPodManagerImplementation;
+    DelayedWithdrawalRouter public delayedWithdrawalRouter;
+    DelayedWithdrawalRouter public delayedWithdrawalRouterImplementation;
+    UpgradeableBeacon public eigenPodBeacon;
+    EigenPod public eigenPodImplementation;
+    StrategyBase public baseStrategyImplementation;
+
+    EmptyContract public emptyContract;
+
+    // the ETH2 deposit contract -- if not on mainnet, we deploy a mock as stand-in
+    IETHPOSDeposit public ethPOSDeposit;
+
+    // strategies deployed
+    StrategyBaseTVLLimits[] public deployedStrategyArray;
+
+    function deployEigenLayer(address executorMultisig, address operationsMultisig, address pauserMultisig, StrategyConfig[] memory strategyConfigs) public {
+        require(executorMultisig != address(0), "executorMultisig address not configured correctly!");
+        require(operationsMultisig != address(0), "operationsMultisig address not configured correctly!");
+
+        // START RECORDING TRANSACTIONS FOR DEPLOYMENT
+        vm.startBroadcast();
+
+        // deploy proxy admin for ability to upgrade proxy contracts
+        eigenLayerProxyAdmin = new ProxyAdmin();
+
+        //deploy pauser registry
+        {
+            address[] memory pausers = new address[](3);
+            pausers[0] = executorMultisig;
+            pausers[1] = operationsMultisig;
+            pausers[2] = pauserMultisig;
+            eigenLayerPauserReg = new PauserRegistry(pausers, executorMultisig);
+        }
+
+        /**
+         * First, deploy upgradeable proxy contracts that **will point** to the implementations. Since the implementation contracts are
+         * not yet deployed, we give these proxies an empty contract as the initial implementation, to act as if they have no code.
+         */
+        emptyContract = new EmptyContract();
+        delegation = DelegationManager(
+            address(new TransparentUpgradeableProxy(address(emptyContract), address(eigenLayerProxyAdmin), ""))
+        );
+        strategyManager = StrategyManager(
+            address(new TransparentUpgradeableProxy(address(emptyContract), address(eigenLayerProxyAdmin), ""))
+        );
+        slasher = Slasher(
+            address(new TransparentUpgradeableProxy(address(emptyContract), address(eigenLayerProxyAdmin), ""))
+        );
+        eigenPodManager = EigenPodManager(
+            address(new TransparentUpgradeableProxy(address(emptyContract), address(eigenLayerProxyAdmin), ""))
+        );
+        delayedWithdrawalRouter = DelayedWithdrawalRouter(
+            address(new TransparentUpgradeableProxy(address(emptyContract), address(eigenLayerProxyAdmin), ""))
+        );
+
+        // ETH POS deposit is 0 address
+        eigenPodImplementation = new EigenPod(
+            ethPOSDeposit,
+            delayedWithdrawalRouter,
+            eigenPodManager,
+            31 ether
+        );
+
+        eigenPodBeacon = new UpgradeableBeacon(address(eigenPodImplementation));
+
+        // Second, deploy the *implementation* contracts, using the *proxy contracts* as inputs
+        delegationImplementation = new DelegationManager(strategyManager, slasher);
+        strategyManagerImplementation = new StrategyManager(delegation, eigenPodManager, slasher);
+        slasherImplementation = new Slasher(strategyManager, delegation);
+        eigenPodManagerImplementation = new EigenPodManager(ethPOSDeposit, eigenPodBeacon, strategyManager, slasher);
+        delayedWithdrawalRouterImplementation = new DelayedWithdrawalRouter(eigenPodManager);
+
+        // Third, upgrade the proxy contracts to use the correct implementation contracts and initialize them.
+        eigenLayerProxyAdmin.upgradeAndCall(
+            TransparentUpgradeableProxy(payable(address(delegation))),
+            address(delegationImplementation),
+            abi.encodeWithSelector(
+                DelegationManager.initialize.selector,
+                executorMultisig,
+                eigenLayerPauserReg,
+                0
+            )
+        );
+        eigenLayerProxyAdmin.upgradeAndCall(
+            TransparentUpgradeableProxy(payable(address(strategyManager))),
+            address(strategyManagerImplementation),
+            abi.encodeWithSelector(
+                StrategyManager.initialize.selector,
+                executorMultisig,
+                operationsMultisig,
+                eigenLayerPauserReg,
+                0,
+                0
+            )
+        );
+        eigenLayerProxyAdmin.upgradeAndCall(
+            TransparentUpgradeableProxy(payable(address(slasher))),
+            address(slasherImplementation),
+            abi.encodeWithSelector(
+                Slasher.initialize.selector,
+                executorMultisig,
+                eigenLayerPauserReg,
+                0
+            )
+        );
+        eigenLayerProxyAdmin.upgradeAndCall(
+            TransparentUpgradeableProxy(payable(address(eigenPodManager))),
+            address(eigenPodManagerImplementation),
+            abi.encodeWithSelector(
+                EigenPodManager.initialize.selector,
+                type(uint256).max,
+                IBeaconChainOracle(address(0)),
+                executorMultisig,
+                eigenLayerPauserReg,
+                0
+            )
+        );
+        eigenLayerProxyAdmin.upgradeAndCall(
+            TransparentUpgradeableProxy(payable(address(delayedWithdrawalRouter))),
+            address(delayedWithdrawalRouterImplementation),
+            abi.encodeWithSelector(DelayedWithdrawalRouter.initialize.selector,
+                executorMultisig,
+                eigenLayerPauserReg,
+                0,
+                0
+            )
+        );
+
+        // deploy StrategyBaseTVLLimits contract implementation
+        baseStrategyImplementation = new StrategyBaseTVLLimits(strategyManager);
+        // create upgradeable proxies that each point to the implementation and initialize them
+        for (uint256 i = 0; i < strategyConfigs.length; ++i) {
+            deployedStrategyArray.push(
+                StrategyBaseTVLLimits(address(
+                    new TransparentUpgradeableProxy(
+                        address(baseStrategyImplementation),
+                        address(eigenLayerProxyAdmin),
+                        abi.encodeWithSelector(StrategyBaseTVLLimits.initialize.selector, strategyConfigs[i].maxPerDeposit, strategyConfigs[i].maxDeposits, IERC20(strategyConfigs[i].tokenAddress), eigenLayerPauserReg)
+                    )
+                ))
+            );
+        }
+
+        eigenLayerProxyAdmin.transferOwnership(executorMultisig);
+        eigenPodBeacon.transferOwnership(executorMultisig);
+
+        // STOP RECORDING TRANSACTIONS FOR DEPLOYMENT
+        vm.stopBroadcast();
+    }
+}
