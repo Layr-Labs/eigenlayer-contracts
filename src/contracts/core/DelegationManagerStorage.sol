293d0d6e3c7ddbca914fab5af03df9c69ab0d969
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-07-19 16:02:54 -0700

    correct storage gap size

--- src/contracts/core/DelegationManagerStorage.sol
+++ src/contracts/core/DelegationManagerStorage.sol
@@ -75,5 +75,5 @@ abstract contract DelegationManagerStorage is IDelegationManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[45] private __gap;
+    uint256[44] private __gap;
 }
