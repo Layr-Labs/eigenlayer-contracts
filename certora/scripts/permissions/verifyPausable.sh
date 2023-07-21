87e50755bb58151c8d7b45300ba1bfaa692ea600
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-07-21 12:21:54 -0700

    update scripts + YAML for Certora Prover
    hopefully this works. at minimum this is a temporary shift to the specs in the /specs2/ folder before we deprecate the old CVL1 specs

--- certora/scripts/permissions/verifyPausable.sh
+++ certora/scripts/permissions/verifyPausable.sh
@@ -3,13 +3,14 @@ then
     RULE="--rule $2"
 fi
 
+solc-select use 0.8.12 
 
 certoraRun certora/harnesses/PausableHarness.sol \
     certora/munged/permissions/PauserRegistry.sol \
-    --verify PausableHarness:certora/specs/permissions/Pausable.spec \
+    --verify PausableHarness:certora/specs2/permissions/Pausable.spec \
     --optimistic_loop \
     --send_only \
-    --settings -optimisticFallback=true,-recursionErrorAsAssert=false,-recursionEntryLimit=3 \
+    --prover_args '-optimisticFallback true -recursionErrorAsAssert false -recursionEntryLimit 3' \
     --loop_iter 3 \
     --link PausableHarness:pauserRegistry=PauserRegistry \
     $RULE \
