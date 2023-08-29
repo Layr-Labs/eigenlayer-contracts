4c9fe821cd050e1227a4c841936f60c67dd27668
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-08-29 11:59:02 -0700

    remove 'send_only' flag from scripts
    should result in CI failing when properties fail, instead of passing as long as the script runs at all.

--- certora/scripts/core/verifySlasher.sh
+++ certora/scripts/core/verifySlasher.sh
@@ -11,7 +11,6 @@ certoraRun certora/harnesses/SlasherHarness.sol \
     certora/munged/core/StrategyManager.sol certora/munged/permissions/PauserRegistry.sol \
     --verify SlasherHarness:certora/specs/core/Slasher.spec \
     --optimistic_loop \
-    --send_only \
     --prover_args '-optimisticFallback true -recursionErrorAsAssert false -recursionEntryLimit 3' \
     --loop_iter 3 \
     --link SlasherHarness:delegation=DelegationManager \
--- certora/scripts/strategies/verifyStrategyBase.sh
+++ certora/scripts/strategies/verifyStrategyBase.sh
@@ -12,7 +12,6 @@ certoraRun certora/munged/strategies/StrategyBase.sol \
     certora/munged/core/Slasher.sol \
     --verify StrategyBase:certora/specs/strategies/StrategyBase.spec \
     --optimistic_loop \
-    --send_only \
     --prover_args '-optimisticFallback true -recursionErrorAsAssert false -recursionEntryLimit 3' \
     --loop_iter 3 \
     --packages @openzeppelin=lib/openzeppelin-contracts @openzeppelin-upgrades=lib/openzeppelin-contracts-upgradeable \
