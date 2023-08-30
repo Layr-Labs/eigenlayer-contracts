d882f4ea67142c15e450852c4d6a112ec38ad2b1
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-08-30 13:38:17 -0700

    reduce `loop_iter` for slasher script to 2
    Checking the `recordStakeUpdate` function burns through a ton of cycles; this makes it a bit faster.

--- certora/scripts/core/verifySlasher.sh
+++ certora/scripts/core/verifySlasher.sh
@@ -12,7 +12,7 @@ certoraRun certora/harnesses/SlasherHarness.sol \
     --verify SlasherHarness:certora/specs/core/Slasher.spec \
     --optimistic_loop \
     --prover_args '-optimisticFallback true -recursionErrorAsAssert false -recursionEntryLimit 3' \
-    --loop_iter 3 \
+    --loop_iter 2 \
     --link SlasherHarness:delegation=DelegationManager \
     $RULE \
     --packages @openzeppelin=lib/openzeppelin-contracts @openzeppelin-upgrades=lib/openzeppelin-contracts-upgradeable \
