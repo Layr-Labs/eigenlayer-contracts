a21e923877aa2c85ad746fc009e69c723b1c1e8e
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-09-05 13:04:51 -0700

    fix to comment wording

--- src/contracts/libraries/BitmapUtils.sol
+++ src/contracts/libraries/BitmapUtils.sol
@@ -36,7 +36,7 @@ library BitmapUtils {
         // initialize an empty uint256 to be used as a bitmask inside the loop
         uint256 bitMask;
 
-        // perform the 0-th loop iteration with the ordering check *omitted* (since it is unnecessary / will always pass)
+        // perform the 0-th loop iteration with the duplicate check *omitted* (since it is unnecessary / will always pass)
         // construct a single-bit mask from the numerical value of the 0th byte of the array, and immediately add it to the bitmap
         bitmap = uint256(1 << uint8(bytesArray[0]));
 
