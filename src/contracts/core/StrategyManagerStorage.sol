feb53c860b20f3d3f164b4359f3ec59989347c9b
Siddhartha Jagannath <60323455+Sidu28@users.noreply.github.com>
2023-09-20 17:26:29 -0700

    fixed sotrage gap in SM storage

--- src/contracts/core/StrategyManagerStorage.sol
+++ src/contracts/core/StrategyManagerStorage.sol
@@ -81,5 +81,5 @@ abstract contract StrategyManagerStorage is IStrategyManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[42] private __gap;
+    uint256[40] private __gap;
 }
