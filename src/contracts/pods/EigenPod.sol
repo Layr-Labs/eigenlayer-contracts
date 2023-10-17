0e525f6c328ba60e746080b01305f5ff27bacd5c
sidu28 <60323455+Sidu28@users.noreply.github.com>
2023-10-17 11:51:12 -0700

    fixed storage gap

--- src/contracts/pods/EigenPod.sol
+++ src/contracts/pods/EigenPod.sol
@@ -775,5 +775,5 @@ contract EigenPod is IEigenPod, Initializable, ReentrancyGuardUpgradeable, Eigen
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[42] private __gap;
+    uint256[44] private __gap;
 }
