4c349dc5b9c8a006fd0237300ff425d0b469e2d4
Yash Patil <ypatil12@gmail.com>
2023-10-24 17:30:20 -0400

    skeleton refactor

--- src/contracts/pods/EigenPodManager.sol
+++ src/contracts/pods/EigenPodManager.sol
@@ -142,13 +142,13 @@ contract EigenPodManager is
      * result in the `podOwner` incurring a "share deficit". This behavior prevents a Staker from queuing a withdrawal which improperly removes excessive
      * shares from the operator to whom the staker is delegated.
      * @dev Reverts if `shares` is not a whole Gwei amount
+     * @dev The delegation manager validates that the podOwner is not address(0)
      */
     function removeShares(
         address podOwner, 
         uint256 shares
     ) external onlyDelegationManager {
-        require(podOwner != address(0), "EigenPodManager.removeShares: podOwner cannot be zero address");
-        require(int256(shares) >= 0, "EigenPodManager.removeShares: shares amount is negative");
+        require(int256(shares) >= 0, "EigenPodManager.removeShares: shares cannot be negative");
         require(shares % GWEI_TO_WEI == 0, "EigenPodManager.removeShares: shares must be a whole Gwei amount");
         int256 updatedPodOwnerShares = podOwnerShares[podOwner] - int256(shares);
         require(updatedPodOwnerShares >= 0, "EigenPodManager.removeShares: cannot result in pod owner having negative shares");
