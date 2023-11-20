877ea71f78a1bb41b0484eaf1e41b1ea0f171c84
Yash Patil <ypatil12@gmail.com>
2023-11-20 16:13:45 -0500

    test: add scenario 2 tests

---
+++ src/test/integration/tests/utils.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.12;
+
+import "src/test/integration/IntegrationBase.t.sol";
+import "src/test/integration/User.t.sol";
+
+/// @notice Contract that provides utility functions to reuse common test blocks
+contract IntegrationTestUtils {
+    
+}
