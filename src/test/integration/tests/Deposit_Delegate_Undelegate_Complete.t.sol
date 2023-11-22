6ffbc5849a52935b35b44dabd08f09eaf79cf738
Yash Patil <ypatil12@gmail.com>
2023-11-22 11:37:33 -0500

    test: add scenario 3

--- src/test/integration/tests/Deposit_Delegate_Undelegate_Complete.t.sol
+++ src/test/integration/tests/Deposit_Delegate_Undelegate_Complete.t.sol
@@ -114,6 +114,105 @@ contract Integration_Deposit_Delegate_Undelegate_Complete is IntegrationTestUtil
         assertWithdrawalAsSharesState(staker, expectedWithdrawal, strategies, shares);
     }
 
+    function testFuzz_deposit_delegate_forceUndelegate_completeAsTokens(uint24 _random) public {
+        // When new Users are created, they will choose a random configuration from these params: 
+        _configRand({
+            _randomSeed: _random,
+            _assetTypes: HOLDS_LST,
+            _userTypes: DEFAULT | SIGNED_METHODS
+        });
+
+        /// 0. Create an operator and a staker with:
+        // - some nonzero underlying token balances
+        // - corresponding to a random number of strategies
+        //
+        // ... check that the staker has no deleagatable shares and isn't delegated
+
+        (
+            User staker,
+            IStrategy[] memory strategies, 
+            uint[] memory tokenBalances
+        ) = _newRandomStaker();
+        (User operator, ,) = _newRandomOperator();
+        uint[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
+
+        assert_HasNoDelegatableShares(staker, "staker should not have delegatable shares before depositing");
+        assertFalse(delegationManager.isDelegated(address(staker)), "staker should not be delegated");
+
+        /// 1. Deposit Into Strategies
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        assertDepositState(staker, strategies, shares);
+
+        // 2. Delegate to an operator
+        staker.delegateTo(operator);
+        assertDelegationState(staker, operator, strategies, shares);
+
+        // 3. Force undelegate
+        IDelegationManager.Withdrawal memory expectedWithdrawal = _getExpectedWithdrawalStruct(staker);
+        bytes32 withdrawalRoot = operator.undelegate(staker);
+        assertUndelegateState(staker, operator, expectedWithdrawal, withdrawalRoot, strategies, shares);
+
+        // 4. Complete withdrawal
+        // Fast forward to when we can complete the withdrawal
+        cheats.roll(block.number + delegationManager.withdrawalDelayBlocks());
+
+        // Complete withdrawal
+        uint[] memory expectedTokens = _calculateExpectedTokens(expectedWithdrawal.strategies, expectedWithdrawal.shares);
+        IERC20[] memory tokens = staker.completeQueuedWithdrawal(expectedWithdrawal, true);
+
+        assertWithdrawalAsTokensState(staker, expectedWithdrawal, strategies, shares, tokens, expectedTokens);
+    }
+
+    function testFuzz_deposit_delegate_forceUndelegate_completeAsShares(uint24 _random) public {
+        // When new Users are created, they will choose a random configuration from these params: 
+        _configRand({
+            _randomSeed: _random,
+            _assetTypes: HOLDS_LST,
+            _userTypes: DEFAULT | SIGNED_METHODS
+        });
+
+        /// 0. Create an operator and a staker with:
+        // - some nonzero underlying token balances
+        // - corresponding to a random number of strategies
+        //
+        // ... check that the staker has no deleagatable shares and isn't delegated
+
+        (
+            User staker,
+            IStrategy[] memory strategies, 
+            uint[] memory tokenBalances
+        ) = _newRandomStaker();
+        (User operator, ,) = _newRandomOperator();
+        uint[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
+
+        assert_HasNoDelegatableShares(staker, "staker should not have delegatable shares before depositing");
+        assertFalse(delegationManager.isDelegated(address(staker)), "staker should not be delegated");
+
+        /// 1. Deposit Into Strategies
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        assertDepositState(staker, strategies, shares);
+
+        // 2. Delegate to an operator
+        staker.delegateTo(operator);
+        assertDelegationState(staker, operator, strategies, shares);
+
+        // 3. Force undelegate
+        IDelegationManager.Withdrawal memory expectedWithdrawal = _getExpectedWithdrawalStruct(staker);
+        bytes32 withdrawalRoot = operator.undelegate(staker);
+        assertUndelegateState(staker, operator, expectedWithdrawal, withdrawalRoot, strategies, shares);
+
+        // 4. Complete withdrawal
+        // Fast forward to when we can complete the withdrawal
+        cheats.roll(block.number + delegationManager.withdrawalDelayBlocks());
+
+        uint[] memory expectedTokens = _calculateExpectedTokens(expectedWithdrawal.strategies, expectedWithdrawal.shares);
+        IERC20[] memory tokens = staker.completeQueuedWithdrawal(expectedWithdrawal, false);
+
+        assertWithdrawalAsSharesState(staker, expectedWithdrawal, strategies, shares);
+    }
+
+
+
     /// @notice Assumes staker and withdrawer are the same
     function _getExpectedWithdrawalStruct(User staker) internal view returns (IDelegationManager.Withdrawal memory) {
         (IStrategy[] memory strategies, uint[] memory shares)
