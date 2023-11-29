010784b35f6ccd76e3632a2934a6975035a32ef7
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-11-29 14:34:57 -0800

    chore: remove `hashing_length_bound` in script
    this should make rules non-vacuous; by using different summaries we can still solve timeouts

--- certora/scripts/pods/verifyEigenPod.sh
+++ certora/scripts/pods/verifyEigenPod.sh
@@ -6,13 +6,15 @@ fi
 solc-select use 0.8.12
 
 certoraRun certora/harnesses/EigenPodHarness.sol \
-    certora/munged/core/DelegationManager.sol certora/munged/pods/EigenPodManager.sol certora/munged/strategies/StrategyBase.sol certora/munged/core/StrategyManager.sol \
+    certora/munged/core/DelegationManager.sol certora/munged/pods/EigenPodManager.sol \
     certora/munged/core/Slasher.sol certora/munged/permissions/PauserRegistry.sol \
+    certora/munged/core/StrategyManager.sol \
+    certora/munged/strategies/StrategyBase.sol \
+    lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol \
     --verify EigenPodHarness:certora/specs/pods/EigenPod.spec \
     --optimistic_loop \
     --prover_args '-optimisticFallback true -recursionEntryLimit 3' \
     --optimistic_hashing \
-    --hashing_length_bound 64 \
     $RULE \
     --loop_iter 1 \
     --packages @openzeppelin=lib/openzeppelin-contracts @openzeppelin-upgrades=lib/openzeppelin-contracts-upgradeable \
