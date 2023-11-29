fa3d3cba9ad674567f37154cf7ed54dc864e3f3a
Yash Patil <ypatil12@gmail.com>
2023-11-29 12:55:03 -0500

    test: scaffold for scenario 4

---
+++ src/test/integration/tests/Deposit_Delegate_Redelegate_Complete.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.12;
+
+import "src/test/integration/IntegrationBase.t.sol";
+import "src/test/integration/User.t.sol";
+import "src/test/integration/tests/utils.t.sol";
+
+contract Integration_Deposit_Delegate_Redelegate_Complete is IntegrationTestUtils {
+    
+}
