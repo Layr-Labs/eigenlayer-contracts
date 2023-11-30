8dff2db735263feac8c3abf19a839be83c60d181
ChaoticWalrus <93558947+ChaoticWalrus@users.noreply.github.com>
2023-11-29 17:30:27 -0800

    chore: add more dispatching and filter parametric contracts

--- certora/scripts/pods/verifyEigenPod.sh
+++ certora/scripts/pods/verifyEigenPod.sh
@@ -11,10 +11,12 @@ certoraRun certora/harnesses/EigenPodHarness.sol \
     certora/munged/core/StrategyManager.sol \
     certora/munged/strategies/StrategyBase.sol \
     lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol \
+    lib/openzeppelin-contracts/contracts/mocks/ERC1271WalletMock.sol \
     --verify EigenPodHarness:certora/specs/pods/EigenPod.spec \
     --optimistic_loop \
     --prover_args '-optimisticFallback true -recursionEntryLimit 3' \
     --optimistic_hashing \
+    --parametric_contracts EigenPodHarness EigenPodManager DelegationManager StrategyManager \
     $RULE \
     --loop_iter 1 \
     --packages @openzeppelin=lib/openzeppelin-contracts @openzeppelin-upgrades=lib/openzeppelin-contracts-upgradeable \
