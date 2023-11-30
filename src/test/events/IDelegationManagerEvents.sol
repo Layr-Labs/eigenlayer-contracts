5fd029069b47bf1632ec49b71533045cf00a45cd
Michael Sun <35479365+8sunyuan@users.noreply.github.com>
2023-11-30 12:07:23 -0500

    Test: DelegationManager unit tests refactor (#332)
    * refactor: initial changes
    
    - Commented out and moved 'integration-like' withdrawal tests to Delegation.t.sol
    - import paths are absolute
    - fixed solidity version
    - Inheriting EigenLayerUnitTestSetup helper contract now (deploys mocks contracts, pauserRegistry, etc)
    
    * test: initial tree file
    
    * refactor: revert tree file back to # syntax while wip
    
    * test: update setup file; add initialization + setter tests
    
    * test: minor tree file updates
    
    * test: adding to tree file
    
    * test: fixed `mananger` typo
    
    * test: add register operator cases
    
    * test: add modify operator tests
    
    * test: add share adjustment tests
    
    * test: add undelegate unit tests
    
    * test: update tree file for withdrawals
    
    * test: delegateTo unit tests
    
    * fix: error from changed internal helper
    
    * test: delegateToBySignature unit tests
    
    * fix: update tree file
    
    * test: minor cleanup
    
    * test: add pausing tests
    
    * test: remove unused helper functions
    
    * test: update all tests to use defaultOperator
    
    * test: remove old tests and formatting
    
    - formatted tree file
    - formatted unit test file
    - removed old tests that have been readded and refactored
    - removed bytes casting for revert strings
    
    test: remove getPods file
    
    * fix: remove duplicated logic
    
    - removed assumes out of internal helpers
    - removed a lot of dup tree branching for readability
    - small nit fixes
    
    * fix: delegateToBySignature tree cleanup
    
    ---------
    
    Co-authored-by: Yash Patil <ypatil12@gmail.com>

---
+++ src/test/events/IDelegationManagerEvents.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.12;
+
+import "src/contracts/interfaces/IDelegationManager.sol";
+import "src/test/mocks/StakeRegistryStub.sol";
+
+interface IDelegationManagerEvents {
+    /// @notice Emitted when the StakeRegistry is set
+    event StakeRegistrySet(IStakeRegistryStub stakeRegistry);
+
+    // @notice Emitted when a new operator registers in EigenLayer and provides their OperatorDetails.
+    event OperatorRegistered(address indexed operator, IDelegationManager.OperatorDetails operatorDetails);
+
+    // @notice Emitted when an operator updates their OperatorDetails to @param newOperatorDetails
+    event OperatorDetailsModified(address indexed operator, IDelegationManager.OperatorDetails newOperatorDetails);
+
+    /**
+     * @notice Emitted when @param operator indicates that they are updating their MetadataURI string
+     * @dev Note that these strings are *never stored in storage* and are instead purely emitted in events for off-chain indexing
+     */
+    event OperatorMetadataURIUpdated(address indexed operator, string metadataURI);
+
+    /// @notice Emitted whenever an operator's shares are increased for a given strategy
+    event OperatorSharesIncreased(address indexed operator, address staker, IStrategy strategy, uint256 shares);
+
+    /// @notice Emitted whenever an operator's shares are decreased for a given strategy
+    event OperatorSharesDecreased(address indexed operator, address staker, IStrategy strategy, uint256 shares);
+
+    // @notice Emitted when @param staker delegates to @param operator.
+    event StakerDelegated(address indexed staker, address indexed operator);
+
+    // @notice Emitted when @param staker undelegates from @param operator.
+    event StakerUndelegated(address indexed staker, address indexed operator);
+
+    /// @notice Emitted when @param staker is undelegated via a call not originating from the staker themself
+    event StakerForceUndelegated(address indexed staker, address indexed operator);
+
+    /**
+     * @notice Emitted when a new withdrawal is queued.
+     * @param withdrawalRoot Is the hash of the `withdrawal`.
+     * @param withdrawal Is the withdrawal itself.
+     */
+    event WithdrawalQueued(bytes32 withdrawalRoot, IDelegationManager.Withdrawal withdrawal);
+
+    /// @notice Emitted when a queued withdrawal is completed
+    event WithdrawalCompleted(bytes32 withdrawalRoot);
+
+    /// @notice Emitted when a queued withdrawal is *migrated* from the StrategyManager to the DelegationManager
+    event WithdrawalMigrated(bytes32 oldWithdrawalRoot, bytes32 newWithdrawalRoot);
+
+    /// @notice Emitted when the `withdrawalDelayBlocks` variable is modified from `previousValue` to `newValue`.
+    event WithdrawalDelayBlocksSet(uint256 previousValue, uint256 newValue);
+
+}
