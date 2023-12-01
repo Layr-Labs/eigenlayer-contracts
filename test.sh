acb3e8ea1594238eeacfa814cd6d2c8b7ae33d07
gpsanant <gpsanant@gmail.com>
2023-11-30 16:41:28 -0800

    decimal checking script and added config for mainnet

--- test.sh
+++ test.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-source .env
+# Define the RPC URL
+RPC_URL="https://rpc.flashbots.net" # Replace with your actual RPC URL
 
-operator1address=$(cast w a --private-key $OPERATOR_1)
-operator2address=$(cast w a --private-key $OPERATOR_2)
-operator3address=$(cast w a --private-key $OPERATOR_3)
-operator4address=$(cast w a --private-key $OPERATOR_4)
+# List of addresses
+declare -a addresses=(
+    "0xf1C9acDc66974dFB6dEcB12aA385b9cD01190E38"
+    "0xf951E335afb289353dc249e82926178EaC7DEd78"
+    "0x856c4Efb76C1D1AE02e20CEB03A2A6a08b0b8dC3"
+    "0xA35b1B31Ce002FBF2058D22F30f95D405200A15b"
+    "0xa2E3356610840701BDf5611a53974510Ae27E2e1"
+    "0xE95A203B1a91a908F9B9CE46459d101078c2c3cb"
+    "0xac3E018457B222d93114458476f3E3416Abbe38F"
+    "0x8c1bed5b9a0928467c9b1341da1d7bd5e10b6549"
+)
 
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_1 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "registerAsOperator((address,address,uint32), string)" "($operator1address,0x0000000000000000000000000000000000000000,10)" "https://operator.com/123"
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_2 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "registerAsOperator((address,address,uint32), string)" "($operator2address,0x0000000000000000000000000000000000000000,0)" "https://123qwe.com/opop"
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_3 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "registerAsOperator((address,address,uint32), string)" "($operator3address,0x0000000000000000000000000000000000000000,0)" "https://geezlouise.com/13"
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_4 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "registerAsOperator((address,address,uint32), string)" "($operator4address,0x0000000000000000000000000000000000000000,0)" "https://urmama.com/rich_dank"
-
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_1 0x78697cd4EE4BdE0514fE8a7C61E8cB1A152B5d78 "registerOperatorWithCoordinator(bytes memory, bytes calldata)" "" ""
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_2 0x78697cd4EE4BdE0514fE8a7C61E8cB1A152B5d78 "registerOperatorWithCoordinator(bytes memory, bytes calldata)" "" ""
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_3 0x78697cd4EE4BdE0514fE8a7C61E8cB1A152B5d78 "registerOperatorWithCoordinator(bytes memory, bytes calldata)" "" ""
-# cast send --rpc-url $RPC_URL --private-key $OPERATOR_4 0x78697cd4EE4BdE0514fE8a7C61E8cB1A152B5d78 "registerOperatorWithCoordinator(bytes memory, bytes calldata)" "" ""
-
-cast send --rpc-url $RPC_URL --private-key $OPERATOR_1 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "updateOperatorMetadataURI(string calldata)" "https://operator-metadata.s3.amazonaws.com/operator_1.json"
-cast send --rpc-url $RPC_URL --private-key $OPERATOR_2 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "updateOperatorMetadataURI(string calldata)" "https://operator-metadata.s3.amazonaws.com/operator_2.json"
-cast send --rpc-url $RPC_URL --private-key $OPERATOR_3 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "updateOperatorMetadataURI(string calldata)" "https://operator-metadata.s3.amazonaws.com/operator_3.json"
-cast send --rpc-url $RPC_URL --private-key $OPERATOR_4 0x45b4c4DAE69393f62e1d14C5fe375792DF4E6332 "updateOperatorMetadataURI(string calldata)" "https://operator-metadata.s3.amazonaws.com/operator_4.json"
+# Loop through each address and call the cast command
+for address in "${addresses[@]}"
+do
+    cast call --rpc-url $RPC_URL "$address" "decimals()"
+done
