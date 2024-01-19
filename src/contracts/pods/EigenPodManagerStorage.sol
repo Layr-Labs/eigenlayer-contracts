3c11f8e1919bb6a468051f199e35ce78fce2b48c
SiddyJ <kachapah@gmail.com>
2024-01-18 18:57:40 -0800

    fixed gap size

--- src/contracts/pods/EigenPodManagerStorage.sol
+++ src/contracts/pods/EigenPodManagerStorage.sol
@@ -85,5 +85,5 @@ abstract contract EigenPodManagerStorage is IEigenPodManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[45] private __gap;
+    uint256[44] private __gap;
 }
