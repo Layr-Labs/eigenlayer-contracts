95df70ed83d055b01b609b81e3926da63c08cb9e
8sunyuan <michaelsun97@gmail.com>
2024-02-02 13:12:16 -0500

    feat: add view for stake updates

--- src/test/mocks/DelegationManagerMock.sol
+++ src/test/mocks/DelegationManagerMock.sol
@@ -87,6 +87,11 @@ contract DelegationManagerMock is IDelegationManager, Test {
         return 0;
     }
 
+    function getOperatorShares(
+        address operator,
+        IStrategy[] memory strategies
+    ) external view returns (uint256[] memory) {}
+
     function getWithdrawalDelay(IStrategy[] calldata /*strategies*/) public view returns (uint256) {
         return 0;
     }
