82def95b882436960a77d6479f3be21f25777ac4
SiddyJ <kachapah@gmail.com>
2024-02-05 17:15:55 -0800

    fixed comment

--- src/contracts/libraries/BeaconChainProofs.sol
+++ src/contracts/libraries/BeaconChainProofs.sol
@@ -320,10 +320,9 @@ library BeaconChainProofs {
 
         {
             /**
-             * Next we verify the withdrawal fields against the blockRoot:
-             * First we compute the withdrawal_index relative to the blockRoot by concatenating the indexes of all the
-             * intermediate root indexes from the bottom of the sub trees (the withdrawal container) to the top, the blockRoot.
-             * Then we calculate merkleize the withdrawalFields container to calculate the the withdrawalRoot.
+             * Next we verify the withdrawal fields against the executionPayloadRoot:
+             * First we compute the withdrawal_index, then we calculate merkleize the 
+             * withdrawalFields container to calculate the the withdrawalRoot.
              * Finally we verify the withdrawalRoot against the executionPayloadRoot.
              *
              *
