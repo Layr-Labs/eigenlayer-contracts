931119a22d274e4ce927ccd03971dce1f6b76704
Michael Sun <35479365+8sunyuan@users.noreply.github.com>
2024-02-06 14:50:44 -0500

    feat: view func for avssync (#423)

--- src/test/mocks/DelegationManagerMock.sol
+++ src/test/mocks/DelegationManagerMock.sol
@@ -86,6 +86,11 @@ contract DelegationManagerMock is IDelegationManager, Test {
     function strategyWithdrawalDelayBlocks(IStrategy /*strategy*/) external pure returns (uint256) {
         return 0;
     }
+    
+    function getOperatorShares(
+        address operator,
+        IStrategy[] memory strategies
+    ) external view returns (uint256[] memory) {}
 
     function getWithdrawalDelay(IStrategy[] calldata /*strategies*/) public pure returns (uint256) {
         return 0;
