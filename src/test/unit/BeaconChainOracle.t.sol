d327f5918961fb28fe9486a105cdb5e938d103d5
SiddyJ <kachapah@gmail.com>
2024-02-22 20:57:59 -0800

    fix: fixed proof

--- src/test/unit/BeaconChainOracle.t.sol
+++ src/test/unit/BeaconChainOracle.t.sol
@@ -18,17 +18,17 @@ contract BeaconChainOracleTest is Test {
         oracle = new BeaconChainOracle();
     }
 
-    function testCallOracleForkTest() public {
-       emit log_named_uint("timestamp", block.timestamp);
-        bytes32 blockRoot = oracle.timestampToBlockRoot(block.timestamp);
+    // function testCallOracleForkTest() public {
+    //    emit log_named_uint("timestamp", block.timestamp);
+    //     bytes32 blockRoot = oracle.timestampToBlockRoot(block.timestamp);
 
-    }
+    // }
 
-    function testCallOracleForkTestFromEOA() public {
-        address eoa = address(0x123);
-        cheats.startPrank(eoa);
-        emit log_named_uint("timestamp", block.timestamp);
-        (bool success, bytes memory data) = address(0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02).call(abi.encodePacked(block.timestamp));
-        require(success, "Precompile call failed");
-    }
+    // function testCallOracleForkTestFromEOA() public {
+    //     address eoa = address(0x123);
+    //     cheats.startPrank(eoa);
+    //     emit log_named_uint("timestamp", block.timestamp);
+    //     (bool success, bytes memory data) = address(0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02).call(abi.encodePacked(block.timestamp));
+    //     require(success, "Precompile call failed");
+    // }
 }
