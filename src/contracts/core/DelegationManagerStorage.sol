e42bc42464bae0fa59d551ada84062e950985cc4
8sunyuan <michaelsun97@gmail.com>
2024-03-06 14:11:59 -0500

    fix: delegation storage gap

--- src/contracts/core/DelegationManagerStorage.sol
+++ src/contracts/core/DelegationManagerStorage.sol
@@ -112,5 +112,5 @@ abstract contract DelegationManagerStorage is IDelegationManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[39] private __gap;
+    uint256[40] private __gap;
 }
