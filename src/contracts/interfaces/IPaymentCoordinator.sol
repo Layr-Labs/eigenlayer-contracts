4341741e9d7d6855c2b46ae67dca3970eb240f8d
8sunyuan <michaelsun97@gmail.com>
2024-03-30 14:08:46 -0400

    fix: add activationDelay and event comments

--- src/contracts/interfaces/IPaymentCoordinator.sol
+++ src/contracts/interfaces/IPaymentCoordinator.sol
@@ -63,18 +63,21 @@ interface IPaymentCoordinator {
 
     /// EVENTS ///
 
+    /// @notice emitted when an AVS creates a valid RangePayment
     event RangePaymentCreated(
         address indexed avs,
         uint256 indexed paymentNonce,
         bytes32 indexed rangePaymentHash,
         RangePayment rangePayment
     );
+    /// @notice emitted when a valid RangePayment is created for all stakers by a valid submitter
     event RangePaymentForAllCreated(
         address indexed submitter,
         uint256 indexed paymentNonce,
         bytes32 indexed rangePaymentHash,
         RangePayment rangePayment
     );
+    /// @notice paymentUpdater is responsible for submiting DistributionRoots, only owner can set paymentUpdater
     event PaymentUpdaterSet(address indexed oldPaymentUpdater, address indexed newPaymentUpdater);
     event PayAllForRangeSubmitterSet(
         address indexed payAllForRangeSubmitter,
@@ -85,6 +88,7 @@ interface IPaymentCoordinator {
     event CalculationIntervalSecondsSet(uint64 oldCalculationIntervalSeconds, uint64 newCalculationIntervalSeconds);
     event GlobalCommissionBipsSet(uint16 oldGlobalCommissionBips, uint16 newGlobalCommissionBips);
     event ClaimerForSet(address indexed earner, address indexed oldClaimer, address indexed claimer);
+    /// @notice rootIndex is the specific array index of the newly created root in the storage array 
     event DistributionRootSubmitted(
         uint32 indexed rootIndex,
         bytes32 indexed root,
@@ -92,6 +96,8 @@ interface IPaymentCoordinator {
         uint64 paymentCalculationEndTimestamp,
         uint64 activatedAt
     );
+    /// @notice earnerTokenRoot is the specific earner root hash that the claim is proven aganst.
+    /// root is the DistributionRoot of that the earner subtree is included in.
     event PaymentClaimed(bytes32 indexed root, bytes32 indexed earnerTokenRoot, ClaimsTreeMerkleLeaf leaf);
 
     /// VIEW FUNCTIONS ///
