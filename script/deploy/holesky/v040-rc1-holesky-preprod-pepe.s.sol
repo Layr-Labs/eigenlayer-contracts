0979b36cad5fb8ebfa7287b8e6e22ccd621cb896
Alex <18387287+wadealexc@users.noreply.github.com>
2024-07-02 12:25:38 -0400

    chore: upgrade preprod eigenpods (#611)
    * chore: upgrade preprod eigenpods
    
    * chore: remove unneeded logs
    
    * chore: deploy and update deployment addresses

--- script/deploy/holesky/v040-rc1-holesky-preprod-pepe.s.sol
+++ script/deploy/holesky/v040-rc1-holesky-preprod-pepe.s.sol
@@ -6,8 +6,8 @@ import "../../utils/ExistingDeploymentParser.sol";
 /**
  * @notice Script used for upgrading EigenPod and EPM Implementation for Holesky preprod
  * anvil --fork-url $RPC_HOLESKY
- * forge script script/deploy/holesky/EigenPod_Checkpoint_Deploy_Preprod.s.sol --rpc-url http://127.0.0.1:8545 --private-key $PRIVATE_KEY --broadcast -vvvv
- * forge script script/deploy/holesky/EigenPod_Checkpoint_Deploy_Preprod.s.sol --rpc-url $RPC_HOLESKY --private-key $PRIVATE_KEY --verify --broadcast -vvvv
+ * forge script script/deploy/holesky/v040-rc1-holesky-preprod-pepe.s.sol --rpc-url http://127.0.0.1:8545 --private-key $PRIVATE_KEY --broadcast -vvvv
+ * forge script script/deploy/holesky/v040-rc1-holesky-preprod-pepe.s.sol --rpc-url $RPC_HOLESKY --private-key $PRIVATE_KEY --verify --broadcast -vvvv
  */
 contract EigenPod_Checkpoint_Deploy_Preprod is ExistingDeploymentParser {
 
@@ -22,12 +22,12 @@ contract EigenPod_Checkpoint_Deploy_Preprod is ExistingDeploymentParser {
             "script/configs/holesky/eigenlayer_addresses_preprod.config.json"
         );
 
+        emit log_named_address("Deployer Address", msg.sender);
+
         // START RECORDING TRANSACTIONS FOR DEPLOYMENT
         vm.startBroadcast();
 
-        emit log_named_address("Deployer Address", msg.sender);
-
-        _upgradeEigenPodAndEPM();
+        _upgradeEigenPods();
 
         // STOP RECORDING TRANSACTIONS FOR DEPLOYMENT
         vm.stopBroadcast();
@@ -38,32 +38,21 @@ contract EigenPod_Checkpoint_Deploy_Preprod is ExistingDeploymentParser {
         _verifyContractsInitialized();
         _verifyInitializationParams();
 
-        logAndOutputContractAddresses("script/output/holesky/EigenPod_Checkpoint_Deploy_Preprod.output.json");
+        logAndOutputContractAddresses("script/output/holesky/v040-rc1.output.json");
     }
 
     /**
-     * @notice Deploy EigenPod and EPM Implementation for Holesky preprod and upgrade the beacon/proxy
+     * @notice Deploy EigenPod Implementation for Holesky preprod and upgrade the beacon
      */
-    function _upgradeEigenPodAndEPM() internal {
-        // Deploy implementations
-        eigenPodManagerImplementation = new EigenPodManager(
-            IETHPOSDeposit(ETHPOSDepositAddress),
-            eigenPodBeacon,
-            strategyManager,
-            slasher,
-            delegationManager
-        );
+    function _upgradeEigenPods() internal {
+        // Deploy implementation
         eigenPodImplementation = new EigenPod(
             IETHPOSDeposit(ETHPOSDepositAddress),
             eigenPodManager,
             EIGENPOD_GENESIS_TIME
         );
 
-        // upgrade TUPS and UpgradeableBeacon
-        eigenLayerProxyAdmin.upgrade(
-            TransparentUpgradeableProxy(payable(address(eigenPodManager))),
-            address(eigenPodManagerImplementation)
-        );
+        // upgrade UpgradeableBeacon
         eigenPodBeacon.upgradeTo(address(eigenPodImplementation));
     }
 }
