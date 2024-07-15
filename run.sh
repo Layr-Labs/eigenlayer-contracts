b540c270c520b9a2dd093cdc1f7b1f10dbe428e9
Otakar <Otakar@certora.com>
2024-07-15 22:50:40 +0200

    run script updated

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-#certoraRun.py certora/confs/StrategyManager_sanity_with_all_default_summaries.conf
-#certoraRun.py certora/confs/StrategyManager_sanity_with_erc20dispatched.conf
-#certoraRun.py certora/confs/StrategyManager_sanity_with_erc20cvl.conf
-#certoraRun.py certora/confs/StrategyManager_sanity.conf
-certoraRun.py certora/confs/StrategyManager_builtin_assertions.conf
+certoraRun.py certora/confs/main.conf
