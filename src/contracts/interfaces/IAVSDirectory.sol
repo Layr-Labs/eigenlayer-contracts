7d1d1e72ec60d96adbcaafdfd8a8b5dbf0cc562f
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2024-07-22 08:43:01 -0400

    feat: operator set migration; completely separate legacy and opset reg (#636)
    * feat: bring back full storage; segregate events
    
    * feat: make operator set creation w/arrays; update interface
    
    * fix: update natspec
    
    * feat: update force dereg func
    
    * chore: fmt
    
    * feat: operator set migration (#637)
    
    * feat: add migration
    
    * test: add unit tests for migration
    
    * chore: format
    
    * fix: check operator set avs in parent func
    
    * fix: compilation
    
    * chore: format
    
    * feat: bring back opertor set struct (#639)

--- src/contracts/interfaces/IAVSDirectory.sol
+++ src/contracts/interfaces/IAVSDirectory.sol
@@ -4,30 +4,46 @@ pragma solidity >=0.5.0;
 import "./ISignatureUtils.sol";
 
 interface IAVSDirectory is ISignatureUtils {
-    struct MemberInfo {
-        uint248 inTotalSets;
-        bool isLegacyOperator;
+    /// @notice Enum representing the registration status of an operator with an AVS.
+    /// @notice Only used by legacy M2 AVSs that have not integrated with operatorSets.
+    enum OperatorAVSRegistrationStatus {
+        UNREGISTERED, // Operator not registered to AVS
+        REGISTERED // Operator registered to AVS
+
+    }
+
+    /// @notice Struct representing an operator set
+    struct OperatorSet {
+        address avs;
+        uint32 operatorSetId;
     }
 
     /// @notice Emitted when an operator set is created by an AVS.
-    event OperatorSetCreated(address indexed avs, uint32 operatorSetId);
+    event OperatorSetCreated(OperatorSet operatorSet);
 
     /**
-     *  @notice Emitted when an operator's registration status with an AVS is updated.
-     *  Specifically, when an operator enters its first operator set for an AVS, or
-     *  when it is removed from the last operator set.
+     *  @notice Emitted when an operator's registration status with an AVS id udpated
+     *  @notice Only used by legacy M2 AVSs that have not integrated with operatorSets.
      */
-    event OperatorAVSRegistrationStatusUpdated(address indexed operator, address indexed avs, bool isLegacyOperator);
+    event OperatorAVSRegistrationStatusUpdated(
+        address indexed operator, address indexed avs, OperatorAVSRegistrationStatus status
+    );
 
     /// @notice Emitted when an operator is added to an operator set.
-    event OperatorAddedToOperatorSet(address operator, address avs, uint32 operatorSetId);
+    event OperatorAddedToOperatorSet(address indexed operator, OperatorSet operatorSet);
 
     /// @notice Emitted when an operator is removed from an operator set.
-    event OperatorRemovedFromOperatorSet(address operator, address avs, uint32 operatorSetId);
+    event OperatorRemovedFromOperatorSet(address indexed operator, OperatorSet operatorSet);
 
     /// @notice Emitted when an AVS updates their metadata URI (Uniform Resource Identifier).
     /// @dev The URI is never stored; it is simply emitted through an event for off-chain indexing.
     event AVSMetadataURIUpdated(address indexed avs, string metadataURI);
+
+    /// @notice Emitted when an AVS migrates to using operator sets.
+    event AVSMigratedToOperatorSets(address indexed avs);
+
+    /// @notice Emitted when an operator is migrated from M2 registration to operator sets.
+    event OperatorMigratedToOperatorSets(address indexed operator, address indexed avs, uint32[] operatorSetIds);
 
     /**
      *
@@ -36,7 +52,40 @@ interface IAVSDirectory is ISignatureUtils {
      */
 
     /**
-     *  @notice Called by AVSs to add an operator to an operator set.
+     * @notice Called by an AVS to create a list of new operatorSets.
+     *
+     * @param operatorSetIds The IDs of the operator set to initialize.
+     *
+     * @dev msg.sender must be the AVS.
+     * @dev The AVS may create operator sets before it becomes an operator set AVS.
+     */
+    function createOperatorSets(uint32[] calldata operatorSetIds) external;
+
+    /**
+     * @notice Sets the AVS as an operator set AVS, preventing legacy M2 operator registrations.
+     *
+     * @dev msg.sender must be the AVS.
+     */
+    function becomeOperatorSetAVS() external;
+
+    /**
+     * @notice Called by an AVS to migrate operators that have a legacy M2 registration to operator sets.
+     *
+     * @param operators The list of operators to migrate
+     * @param operatorSetIds The list of operatorSets to migrate the operators to
+     *
+     * @dev The msg.sender used is the AVS
+     * @dev The operator can only be migrated at most once per AVS
+     * @dev The AVS can no longer register operators via the legacy M2 registration path once it begins migration
+     * @dev The operator is deregistered from the M2 legacy AVS once migrated
+     */
+    function migrateOperatorsToOperatorSets(
+        address[] calldata operators,
+        uint32[][] calldata operatorSetIds
+    ) external;
+
+    /**
+     *  @notice Called by AVSs to add an operator to list of operatorSets.
      *
      *  @param operator The address of the operator to be added to the operator set.
      *  @param operatorSetIds The IDs of the operator sets.
@@ -44,25 +93,12 @@ interface IAVSDirectory is ISignatureUtils {
      *
      *  @dev msg.sender is used as the AVS.
      *  @dev The operator must not have a pending deregistration from the operator set.
-     *  @dev If this is the first operator set in the AVS that the operator is
-     *  registering for, a OperatorAVSRegistrationStatusUpdated event is emitted with
-     *  a REGISTERED status.
      */
     function registerOperatorToOperatorSets(
         address operator,
         uint32[] calldata operatorSetIds,
         ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature
     ) external;
-
-    /**
-     * @notice Called by an operator to deregister from an operator set
-     *
-     * @param avs The address of the AVS to deregister the operator from.
-     * @param operatorSetIds The IDs of the operator sets.
-     *
-     * @dev msg.sender used is the operator
-     */
-    function deregisterFromAVSOperatorSets(address avs, uint32[] calldata operatorSetIds) external;
 
     /**
      *  @notice Called by AVSs to remove an operator from an operator set.
@@ -73,6 +109,24 @@ interface IAVSDirectory is ISignatureUtils {
      *  @dev msg.sender is used as the AVS.
      */
     function deregisterOperatorFromOperatorSets(address operator, uint32[] calldata operatorSetIds) external;
+
+    /**
+     * @notice Called by an operator to deregister from an operator set
+     *
+     * @param operator The operator to deregister from the operatorSets.
+     * @param avs The address of the AVS to deregister the operator from.
+     * @param operatorSetIds The IDs of the operator sets.
+     * @param operatorSignature the signature of the operator on their intent to deregister or empty if the operator itself is calling
+     *
+     * @dev if the operatorSignature is empty, the caller must be the operator
+     * @dev this will likely only be called in case the AVS contracts are in a state that prevents operators from deregistering
+     */
+    function forceDeregisterFromOperatorSets(
+        address operator,
+        address avs,
+        uint32[] calldata operatorSetIds,
+        ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature
+    ) external;
 
     /**
      *  @notice Called by the AVS's service manager contract to register an operator with the AVS.
@@ -120,11 +174,6 @@ interface IAVSDirectory is ISignatureUtils {
      */
     function operatorSaltIsSpent(address operator, bytes32 salt) external view returns (bool);
 
-    function memberInfo(
-        address avs,
-        address operator
-    ) external view returns (uint248 inTotalSets, bool isLegacyOperator);
-
     function isMember(address avs, address operator, uint32 operatorSetId) external view returns (bool);
 
     /**
@@ -151,6 +200,21 @@ interface IAVSDirectory is ISignatureUtils {
      * @param expiry Time after which the approver's signature becomes invalid.
      */
     function calculateOperatorSetRegistrationDigestHash(
+        address avs,
+        uint32[] calldata operatorSetIds,
+        bytes32 salt,
+        uint256 expiry
+    ) external view returns (bytes32);
+
+    /**
+     * @notice Calculates the digest hash to be signed by an operator to force deregister from an operator set.
+     *
+     * @param avs The AVS that operator is deregistering from.
+     * @param operatorSetIds An array of operator set IDs the operator is deregistering from.
+     * @param salt A unique and single use value associated with the approver signature.
+     * @param expiry Time after which the approver's signature becomes invalid.
+     */
+    function calculateOperatorSetForceDeregistrationTypehash(
         address avs,
         uint32[] calldata operatorSetIds,
         bytes32 salt,
