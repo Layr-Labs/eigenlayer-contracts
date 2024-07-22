4dd5f28486a31054f3c89c6fea6b97952f5600d7
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2024-07-22 08:43:01 -0400

    feat: operator set migration; completely separate legacy and opset reg (#636)
    * feat: bring back full storage; segregate events
    
    * feat: make operator set creation w/arrays; update interface
    
    * fix: update natspec
    
    * feat: update force dereg func
    
    * chore: fmt
    
    * feat: operator set migration (#637)
    
    * feat: add migration
    
    * test: add unit tests for migration
    
    * chore: format
    
    * fix: check operator set avs in parent func
    
    * fix: compilation
    
    * chore: format
    
    * feat: bring back opertor set struct (#639)

--- src/test/events/IAVSDirectoryEvents.sol
+++ src/test/events/IAVSDirectoryEvents.sol
@@ -4,12 +4,28 @@ pragma solidity ^0.8.12;
 import "src/contracts/interfaces/IAVSDirectory.sol";
 
 interface IAVSDirectoryEvents {
+    /// @notice Emitted when an operator set is created by an AVS.
+    event OperatorSetCreated(IAVSDirectory.OperatorSet operatorSet);
+
     /**
-     * @notice Emitted when @param avs indicates that they are updating their MetadataURI string
-     * @dev Note that these strings are *never stored in storage* and are instead purely emitted in events for off-chain indexing
+     *  @notice Emitted when an operator's registration status with an AVS id udpated
+     *  @notice Only used by legacy M2 AVSs that have not integrated with operatorSets.
      */
+    event OperatorAVSRegistrationStatusUpdated(address indexed operator, address indexed avs, IAVSDirectory.OperatorAVSRegistrationStatus status);
+
+    /// @notice Emitted when an operator is added to an operator set.
+    event OperatorAddedToOperatorSet(address indexed operator, IAVSDirectory.OperatorSet operatorSet);
+
+    /// @notice Emitted when an operator is removed from an operator set.
+    event OperatorRemovedFromOperatorSet(address indexed operator, IAVSDirectory.OperatorSet operatorSet);
+
+    /// @notice Emitted when an AVS updates their metadata URI (Uniform Resource Identifier).
+    /// @dev The URI is never stored; it is simply emitted through an event for off-chain indexing.
     event AVSMetadataURIUpdated(address indexed avs, string metadataURI);
 
-    /// @notice Emitted when an operator's registration status for an AVS is updated
-    event OperatorAVSRegistrationStatusUpdated(address indexed operator, address indexed avs, bool status);
+    /// @notice Emitted when an AVS migrates to using operator sets
+    event AVSMigratedToOperatorSets(address indexed avs);
+
+    /// @notice Emitted when an operator is migrated from M2 registration to operator sets.
+    event OperatorMigratedToOperatorSets(address indexed operator, address indexed avs, uint32[] operatorSetIds);
 }
