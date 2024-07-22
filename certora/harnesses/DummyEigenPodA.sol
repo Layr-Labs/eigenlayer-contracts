9d1fba325883a926f8210a116b504f74e231d309
Otakar <Otakar@certora.com>
2024-07-22 13:28:41 +0200

    Added harnesses for EigenPod

---
+++ certora/harnesses/DummyEigenPodA.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.12;
+
+import "../../src/contracts/pods/EigenPod.sol";
+import "./EigenPodHarness.sol";
+
+contract DummyEigenPodA is EigenPodHarness {
+    constructor(
+        IETHPOSDeposit _ethPOS,
+        IDelayedWithdrawalRouter _delayedWithdrawalRouter,
+        IEigenPodManager _eigenPodManager,
+        uint64 _MAX_RESTAKED_BALANCE_GWEI_PER_VALIDATOR,
+        uint64 _GENESIS_TIME
+    )
+    EigenPodHarness(_ethPOS, _delayedWithdrawalRouter, _eigenPodManager, _MAX_RESTAKED_BALANCE_GWEI_PER_VALIDATOR, _GENESIS_TIME) {}
+
+}
