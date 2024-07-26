47e4550267af876eb27ef692eed2d2d22c230829
Yash Patil <ypatil12@gmail.com>
2024-07-26 15:23:14 -0400

    fix: strategy unit tests

--- src/test/mocks/Reverter.sol
+++ src/test/mocks/Reverter.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.9;
 
 contract Reverter {
-
     fallback() external {
         revert("Reverter: I am a contract that always reverts");
+    }
+}
+
+contract ReverterWithDecimals is Reverter {
+    function decimals() external pure returns (uint8) {
+        return 18;
     }
 }
