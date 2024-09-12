c29e9e812f552c8b8f172426c971c481f7d10bee
Michael Sun <michaelsun97@gmail.com>
2024-09-12 18:34:22 -0400

    chore: rebase gap fix

--- src/contracts/core/DelegationManagerStorage.sol
+++ src/contracts/core/DelegationManagerStorage.sol
@@ -136,5 +136,5 @@ abstract contract DelegationManagerStorage is IDelegationManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[39] private __gap;
+    uint256[37] private __gap;
 }
