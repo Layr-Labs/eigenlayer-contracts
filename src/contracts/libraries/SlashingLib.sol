1251bb9b6ac8077f7f2d10230b2028fdef274e42
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2024-09-16 17:17:50 -0400

    feat: avoid overflow `SlashingLib`

--- src/contracts/libraries/SlashingLib.sol
+++ src/contracts/libraries/SlashingLib.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.27;
 
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 library SlashingLib {
+    using SlashingLib for uint256;
+
     /// @dev The initial total magnitude for an operator
     uint64 public constant INITIAL_TOTAL_MAGNITUDE = 1e18;
 
-    /// @notice that stakerScalingFactor and totalMagnitude have initial default values to 1e18 as "1"
+    /// @dev that stakerScalingFactor and totalMagnitude have initial default values to 1e18 as "1"
     /// to preserve precision with uint256 math. We use `PRECISION_FACTOR` where these variables are used
     /// and divide to represent as 1
     uint256 public constant PRECISION_FACTOR = 1e18;
@@ -15,7 +19,7 @@ library SlashingLib {
     uint32 public constant DEALLOCATION_DELAY = 17.5 days;
 
     /**
-     * @notice helper pure to calculate the scaledShares to store in queue withdrawal
+     * @dev Helper pure function to calculate the scaledShares to store in queue withdrawal
      * and the shares to decrement from StrategyManager/EigenPodManager
      */
     function calculateSharesToQueueWithdraw(
@@ -24,31 +28,24 @@ library SlashingLib {
         uint64 totalMagnitude
     ) internal pure returns (uint256 sharesToDecrement, uint256 scaledStakerShares) {
         // TODO: DOUBLE CHECK THIS BEHAVIOR
-        // NOTE that to prevent numerator overflow, the max sharesToWithdraw is
-        // x*1e36 <= 2^256-1
-        // => x <= 1.1579e41
-        // however we know that the max shares in a strategy is 1e38-1 and all the ETH on the beaconchain staked
-        // in one eigenPod would be ~3e25. This product here is safe
-        sharesToDecrement =
-            (sharesToWithdraw * PRECISION_FACTOR / stakerScalingFactor) * PRECISION_FACTOR / totalMagnitude;
-        scaledStakerShares = sharesToWithdraw * PRECISION_FACTOR / totalMagnitude;
-        return (sharesToDecrement, scaledStakerShares);
+        sharesToDecrement = sharesToWithdraw.divWad(stakerScalingFactor).divWad(totalMagnitude);
+        scaledStakerShares = sharesToWithdraw.divWad(totalMagnitude);
     }
 
     /**
-     * @notice helper pure to calculate the shares to complete a withdrawal after slashing
+     * @dev helper pure to calculate the shares to complete a withdrawal after slashing
      * We use the totalMagnitude of the delegated operator at the time of withdrawal completion to scale the shares
      * back to real StrategyManager/EigenPodManager shares
      */
     function calculateSharesToCompleteWithdraw(
         uint256 scaledStakerShares,
         uint64 totalMagnitudeAtCompletion
-    ) internal pure returns (uint256 shares) {
-        shares = scaledStakerShares * totalMagnitudeAtCompletion / PRECISION_FACTOR;
+    ) internal pure returns (uint256) {
+        return scaledStakerShares.mulWad(totalMagnitudeAtCompletion);
     }
 
     /**
-     * @notice helper to calculate the withdrawable shares for a staker given their shares and the
+     * @dev helper to calculate the withdrawable shares for a staker given their shares and the
      * current totalMagnitude. `shares` should be the staker's shares in storage in the StrategyManager/EigenPodManager.
      */
     function getWithdrawableShares(
@@ -57,27 +54,27 @@ library SlashingLib {
         uint256 shares,
         uint64 currTotalMagnitude
     ) internal pure returns (uint256) {
-        return (stakerScalingFactor * shares / PRECISION_FACTOR) * currTotalMagnitude / PRECISION_FACTOR;
+        return stakerScalingFactor.mulWad(shares).mulWad(currTotalMagnitude);
     }
 
     /**
-     * @notice helper pure to return scaledShares given shares and current totalMagnitude. Used for
+     * @dev helper pure to return scaledShares given shares and current totalMagnitude. Used for
      * adding/removing staker shares from operatorScaledShares
      */
     function scaleShares(uint256 shares, uint64 totalMagnitude) internal pure returns (uint256) {
-        return shares * PRECISION_FACTOR / totalMagnitude;
+        return shares.divWad(totalMagnitude);
     }
 
     /**
-     * @notice helper pure to return real strategy shares given operator scaledShares and current totalMagnitude.
+     * @dev helper pure to return real strategy shares given operator scaledShares and current totalMagnitude.
      * Used for returning the total delegated shares for an operator and strategy
      */
     function descaleShares(uint256 scaledShares, uint64 totalMagnitude) internal pure returns (uint256) {
-        return scaledShares * totalMagnitude / PRECISION_FACTOR;
+        return scaledShares.mulWad(totalMagnitude);
     }
 
     /**
-     * @notice helper to calculate the new staker scaling factor after adding shares. This is only used
+     * @dev helper to calculate the new staker scaling factor after adding shares. This is only used
      * when a staker is depositing through the StrategyManager or EigenPodManager. A stakers scaling factor
      * is only updated when they have new deposits and their shares are being increased.
      */
@@ -88,23 +85,31 @@ library SlashingLib {
         uint256 existingShares,
         uint256 addedShares
     ) internal pure returns (uint256) {
-        uint256 newStakerScalingFactor;
-
         if (existingShares == 0) {
             // existing shares are 0, meaning no existing delegated shares. In this case, the new staker scaling factor
             // is re-initialized to
-            newStakerScalingFactor = PRECISION_FACTOR / (totalMagnitude);
+            return PRECISION_FACTOR / totalMagnitude;
         } else {
             // TODO: DOUBLE CHECK THIS BEHAVIOR AND OVERFLOWS
             // staker scaling factor is initialized to PRECISION_FACTOR(1e18) and totalMagnitude is initialized to INITIAL_TOTAL_MAGNITUDE(1e18)
             // and is monotonically decreasing. You can deduce that the newStakerScalingFactor will never decrease to less than the PRECISION_FACTOR
             // so this won't round to 0.
-            newStakerScalingFactor = (
-                existingShares * currStakerScalingFactor / PRECISION_FACTOR * totalMagnitude / PRECISION_FACTOR
-                    + addedShares
-            ) / ((existingShares + addedShares) * totalMagnitude / PRECISION_FACTOR);
+            return (existingShares.mulWad(currStakerScalingFactor).mulWad(totalMagnitude) + addedShares)
+                / (existingShares + addedShares).mulWad(totalMagnitude);
         }
+    }
 
-        return newStakerScalingFactor;
+    /// WAD MATH
+
+    /// @dev Multiplys two unsigned intergers that are denominated in wad (18 decimal places).
+    /// Intermediate overflow is avoided, read more here: https://xn--2-umb.com/21/muldiv
+    function mulWad(uint256 a, uint256 b) internal pure returns (uint256) {
+        return Math.mulDiv(a, b, PRECISION_FACTOR);
+    }
+
+    /// @dev Divides two unsigned intergers that are denominated in wad (18 decimal places).
+    /// Intermediate overflow is avoided, read more here: https://xn--2-umb.com/21/muldiv
+    function divWad(uint256 a, uint256 b) internal pure returns (uint256) {
+        return Math.mulDiv(a, PRECISION_FACTOR, b);
     }
 }
