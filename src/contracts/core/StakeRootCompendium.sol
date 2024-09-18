3004840803d6412e6ea8536492ac3270976930ff
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2024-09-18 10:16:28 -0400

    fix: bump pragma -> ^0.8.27

--- src/contracts/core/StakeRootCompendium.sol
+++ src/contracts/core/StakeRootCompendium.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 import "../interfaces/IStrategy.sol";
