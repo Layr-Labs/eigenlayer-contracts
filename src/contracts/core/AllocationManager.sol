4b56b8cef6a92337cd7e83aec5396d8e3b0d2c03
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2024-09-19 12:34:53 -0400

    feat: stakeroot cleanup (#766)
    * chore: forge fmt src/contracts
    
    * nit: organize imports
    
    * fix: bump pragma -> ^0.8.27
    
    * refactor(optimization): significantly reduce sloads/sstores
    
    * refactor: custom errors
    
    * refactor(optimization): significantly reduce sloads/sstores
    
    missed some stuff
    
    * refactor: rename compendium -> manager
    
    * refactor: more storage optmizations
    
    * feat: safe eth transfer helper
    
    * refactor: manager -> compendium
    
    * refactor: variable renaming
    
    * refactor: rename compendium -> manager
    
    * feat: add `proofIntervalSeconds` getter
    
    * feat: fixed accounting bug and refactoring (#767)
    
    * refactor: review reconciliations
    
    * refactor: review reconciliations
    
    * fix: rename colluding param
    
    * fix: types and naming
    
    * fix: revert stakeroot calculation changes
    
    * fix: revert stakeRoot calc views
    
    ---------
    
    Co-authored-by: shotaro <10378902+shotaronowhere@users.noreply.github.com>

--- src/contracts/core/AllocationManager.sol
+++ src/contracts/core/AllocationManager.sol
@@ -423,7 +423,7 @@ contract AllocationManager is
         return uint64(totalMagnitude);
     }
 
-    /// @dev gets the latest total magnitude 
+    /// @dev gets the latest total magnitude
     function _getLatestTotalMagnitudeView(address operator, IStrategy strategy) internal view returns (uint64) {
         (bool exists,, uint224 totalMagnitude) = _totalMagnitudeUpdate[operator][strategy].latestSnapshot();
         if (!exists) {
@@ -441,11 +441,8 @@ contract AllocationManager is
         uint64 totalMagnitude = _getLatestTotalMagnitudeView(operator, strategy);
 
         bytes32 operatorSetKey = _encodeOperatorSet(operatorSet);
-        uint64 currentMagnitude = uint64(
-                _magnitudeUpdate[operator][strategy][operatorSetKey].upperLookupLinear(
-                    uint32(block.timestamp)
-                )
-            );
+        uint64 currentMagnitude =
+            uint64(_magnitudeUpdate[operator][strategy][operatorSetKey].upperLookupLinear(uint32(block.timestamp)));
 
         return (totalMagnitude, currentMagnitude);
     }
@@ -652,7 +649,8 @@ contract AllocationManager is
         uint64[] memory totalMagnitude = new uint64[](strategies.length);
         uint64[] memory allocatedMagnitude = new uint64[](strategies.length);
         for (uint256 i = 0; i < strategies.length; ++i) {
-            (totalMagnitude[i], allocatedMagnitude[i]) = _getTotalAndAllocatedMagnitude(operator, operatorSet, strategies[i]);
+            (totalMagnitude[i], allocatedMagnitude[i]) =
+                _getTotalAndAllocatedMagnitude(operator, operatorSet, strategies[i]);
         }
         return (totalMagnitude, allocatedMagnitude);
     }
