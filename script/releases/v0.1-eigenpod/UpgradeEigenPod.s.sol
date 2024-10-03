84616687a8c155f136d6f465707530dfe993fbfe
Nadir Akhtar <nadirakhtar123@gmail.com>
2024-10-02 20:09:14 -0700

    Craft transactions producing a bytestring

--- script/releases/v0.1-eigenpod/UpgradeEigenPod.s.sol
+++ script/releases/v0.1-eigenpod/UpgradeEigenPod.s.sol
@@ -2,33 +2,30 @@
 pragma solidity ^0.8.12;
 
 import "script/Release_Template.s.sol";
+import {IUpgradeableBeacon} from "script/utils/Interfaces.sol";
 
 contract UpgradeCounter is MultisigBuilder {
 
     function _execute(Addresses memory addrs, Environment memory env, Params memory params) internal override returns (Tx[] memory) {
         Tx[] memory txs = new Tx[](2);
 
-        // txs[0] = Tx({
-        //     to: eigenPodBeacon,
-        //     value: 0,
-        //     data: abi.encodeWithSelector(
-        //         IUpgradeableBeacon.upgradeTo.selector, newEigenPodImpl
-        //     )
-        // });
+        txs[0] = Tx({
+            to: addrs.eigenPod.beacon,
+            value: 0,
+            data: abi.encodeWithSelector(
+                IUpgradeableBeacon.upgradeTo.selector,
+                addrs.eigenPod.pendingImpl
+            )
+        });
 
-        // txs[1] = Tx({
-        //     to: eigenLayerProxyAdmin,
-        //     value: 0,
-        //     data: abi.encodeWithSelector(ProxyAdmin.upgrade.selector, eigenPodManager, newEigenPodManagerImpl)
-        // });
-
-        bytes memory calldata_to_executor; // upgrade data
-
-        Transaction({
+        txs[1] = Tx({
             to: addrs.proxyAdmin,
             value: 0,
-            data: calldata_to_executor,
-            op: EncGnosisSafe.Operation.DelegateCall
+            data: abi.encodeWithSelector(
+                ProxyAdmin.upgrade.selector,
+                addrs.eigenPodManager,
+                addrs.eigenPod.pendingImpl
+            )
         });
 
         return txs;
