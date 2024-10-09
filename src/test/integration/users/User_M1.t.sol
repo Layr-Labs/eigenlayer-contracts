47064428f41cca742a6c0294c711ef680542600b
Yash Patil <ypatil12@gmail.com>
2024-10-09 12:15:10 -0700

    fix: compilation

--- src/test/integration/users/User_M1.t.sol
+++ src/test/integration/users/User_M1.t.sol
@@ -5,6 +5,7 @@ import "src/test/integration/deprecatedInterfaces/mainnet/IEigenPod.sol";
 import "src/test/integration/deprecatedInterfaces/mainnet/IEigenPodManager.sol";
 import "src/test/integration/deprecatedInterfaces/mainnet/IStrategyManager.sol";
 import "src/test/integration/users/User.t.sol";
+import "src/contracts/mixins/SignatureUtils.sol";
 
 interface IUserMainnetForkDeployer {
     function delegationManager() external view returns (DelegationManager);
@@ -108,7 +109,7 @@ contract User_M1_AltMethods is User_M1 {
                     expiry
                 )
             );
-            bytes32 domain_separator = keccak256(abi.encode(EIP712_DOMAIN_TYPEHASH, keccak256(bytes("EigenLayer")), block.chainid, address(strategyManager)));
+            bytes32 domain_separator = strategyManager.domainSeparator();
             bytes32 digestHash =
                 keccak256(abi.encodePacked("\x19\x01", domain_separator, structHash));
             bytes memory signature = bytes(abi.encodePacked(digestHash)); // dummy sig data
