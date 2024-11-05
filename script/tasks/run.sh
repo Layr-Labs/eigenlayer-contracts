8d2a89a49001a7ca8b513edcc9fd28cec30d6242
Grezle <4310551+grezle@users.noreply.github.com>
2024-11-05 15:36:53 +0000

    fix: add additional deposit

--- script/tasks/run.sh
+++ script/tasks/run.sh
@@ -70,7 +70,14 @@ forge script ../tasks/slash_operatorSet.s.sol \
     --sig "run(string memory configFile,address operator,uint32 operatorSetId,uint256 wadToSlash)" \
     -- local/slashing_output.json $SENDER 00000001 0500000000000000000
 
+# Deposit more shares into strategy
+forge script ../tasks/deposit_into_strategy.s.sol \
+    --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast \
+    --sig "run(string memory configFile,address strategy,address token,uint256 amount)" \
+    -- local/slashing_output.json $STRATEGY $TOKEN $DEPOSIT_SHARES
+
 # Fetch current withdrawable shares and nonce
+DEPOSITS=$(cast call $DELEGATION_MANAGER "getDepositedShares(address)(address[],uint256[])" $SENDER "[$STRATEGY]" --rpc-url $RPC_URL | sed -n '2p' | tr -d '[]')
 SHARES=$(cast call $DELEGATION_MANAGER "getWithdrawableShares(address,address[])(uint256[])" $SENDER "[$STRATEGY]" --rpc-url $RPC_URL | sed 's/[][]//g')
 NONCE=$(cast call $DELEGATION_MANAGER "stakerNonce(address)(uint256)" $SENDER --rpc-url $RPC_URL)
 
@@ -93,7 +100,7 @@ forge script ../tasks/complete_withdrawal_from_strategy.s.sol \
     -- local/slashing_output.json $STRATEGY $TOKEN $SHARES $NONCE $WITHDRAWAL_START_BLOCK_NUMBER
 
 # Verification
-FINAL_SHARES=$(cast call $DELEGATION_MANAGER "getWithdrawableShares(address,address[])(uint256[])" $SENDER "[$STRATEGY]" --rpc-url $RPC_URL)
+FINAL_SHARES=$(cast call $DELEGATION_MANAGER "getWithdrawableShares(address,address[])(uint256[])" $SENDER "[$STRATEGY]" --rpc-url $RPC_URL | sed 's/[][]//g')
 FINAL_BALANCE=$(cast call $TOKEN "balanceOf(address)(uint256)" $SENDER --rpc-url $RPC_URL)
 BALANCE_AS_DEC=$(echo "$BALANCE" | awk '{print $1}')
 FINAL_BALANCE_AS_DEC=$(echo "$FINAL_BALANCE" | awk '{print $1}')
@@ -104,7 +111,7 @@ echo -e "==========================\n"
 echo -e "Addresses saved to: $(realpath $(pwd)/../../script/output/local/slashing_output.json)"
 echo -e "\n==========================\n"
 echo "Number of tokens held initially: $BALANCE"
-echo "Number of shares deposited: $DEPOSIT_SHARES"
+echo "Number of shares deposited: $DEPOSITS"
 echo "Number of shares after slashing: $SHARES"
 echo "Number of shares remaining: $FINAL_SHARES"
 echo "Number of tokens held: $FINAL_BALANCE"
