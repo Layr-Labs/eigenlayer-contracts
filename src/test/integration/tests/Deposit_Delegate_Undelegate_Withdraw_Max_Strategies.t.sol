a8c5789a872555a4b14cbcf1df1025039f494a5e
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2024-12-02 16:16:58 -0500

    test: review changes
    - still failing with `WithdrawalNotQueued` when slashing added

--- src/test/integration/tests/Deposit_Delegate_Undelegate_Withdraw_Max_Strategies.t.sol
+++ src/test/integration/tests/Deposit_Delegate_Undelegate_Withdraw_Max_Strategies.t.sol
@@ -32,12 +32,44 @@ contract Integration_Deposit_Delegate_Undelegate_Withdraw_Max_Strategies is
         // 3. Register for operator set
         operator.registerForOperatorSet(operatorSet);
 
+        // 4. Undelegate from an operator
+        Withdrawal[] memory withdrawals = staker.undelegate();
+
+        // 5. Complete withdrawal
+        _rollBlocksForCompleteWithdrawals();
+        staker.completeWithdrawalsAsTokens(withdrawals);
+    }
+
+    function testFuzz_deposit_delegate_allocate_slash_undelegate_withdraw_max_strategies(
+        uint24 _random
+    ) public {
+        // When new Users are created, they will choose a random configuration from these params:
+        _configRand({_randomSeed: _random, _assetTypes: HOLDS_MAX, _userTypes: DEFAULT});
+        _upgradeEigenLayerContracts(); // Upgrade contracts if forkType is not local
+
+        (User staker, IStrategy[] memory strategies, uint256[] memory balances) = _newRandomStaker();
+        (User operator,,) = _newRandomOperator();
+        (AVS avs,) = _newRandomAVS();
+
+        OperatorSet memory operatorSet = avs.createOperatorSet(strategies);
+
+        assertEq(strategies.length, 33, "sanity");
+
+        /// 1. Deposit Into Strategies
+        staker.depositIntoEigenlayer(strategies, balances);
+
+        // 2. Delegate to an operator
+        staker.delegateTo(operator);
+
+        // 3. Register for operator set
+        operator.registerForOperatorSet(operatorSet);
+
         // 4. Allocate to operator set.
         operator.modifyAllocations(operatorSet, _randMagnitudes({sum: 1 ether, len: strategies.length}));
         cheats.roll(block.number + allocationManager.ALLOCATION_CONFIGURATION_DELAY());
 
         // 5. Slash operator
-        avs.slashOperator(operator, operatorSet.id, _randWadToSlash());
+        avs.slashOperator(operator, operatorSet.id, 0.5 ether);
 
         // 6. Undelegate from an operator
         Withdrawal[] memory withdrawals = staker.undelegate();
