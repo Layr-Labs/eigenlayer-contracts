a9aac4e318a010e6ab8d6d03ec0804d820070cab
Nadir Akhtar <nadirakhtar123@gmail.com>
2024-12-04 10:08:57 -0800

    docs: add slashing docs

--- src/contracts/interfaces/IDelegationManager.sol
+++ src/contracts/interfaces/IDelegationManager.sol
@@ -228,13 +228,10 @@ interface IDelegationManager is ISignatureUtils, IDelegationManagerErrors, IDele
     /**
      * @notice Caller delegates their stake to an operator.
      * @param operator The account (`msg.sender`) is delegating its assets to for use in serving applications built on EigenLayer.
-     * @param approverSignatureAndExpiry Verifies the operator approves of this delegation
-     * @param approverSalt A unique single use value tied to an individual signature.
-     * @dev The approverSignatureAndExpiry is used in the event that the operator's `delegationApprover` address is set to a non-zero value.
-     * @dev In the event that `approverSignatureAndExpiry` is not checked, its content is ignored entirely; it's recommended to use an empty input
-     * in this case to save on complexity + gas costs
-     * @dev If the staker delegating has shares in a strategy that the operator was slashed 100% for (the operator's maxMagnitude = 0),
-     * then delegation is blocked and will revert.
+     * @param approverSignatureAndExpiry (optional) Verifies the operator approves of this delegation
+     * @param approverSalt (optional) A unique single use value tied to an individual signature.
+     * @dev The signature/salt are used ONLY if the operator has configured a delegationApprover.
+     * If they have not, these params can be left empty.
      */
     function delegateTo(
         address operator,
@@ -243,14 +240,14 @@ interface IDelegationManager is ISignatureUtils, IDelegationManagerErrors, IDele
     ) external;
 
     /**
-     * @notice Undelegates the staker from the operator who they are delegated to.
-     * Queues withdrawals of all of the staker's withdrawable shares in the StrategyManager (to the staker) and/or EigenPodManager, if necessary.
-     * @param staker The account to be undelegated.
-     * @return withdrawalRoots The roots of the newly queued withdrawals, if a withdrawal was queued. Otherwise just bytes32(0).
+     * @notice Undelegates the staker from their operator and queues a withdrawal for all of their shares
+     * @param staker The account to be undelegated
+     * @return withdrawalRoots The roots of the newly queued withdrawals, if a withdrawal was queued. Returns
+     * an empty array if none was queued.
      *
      * @dev Reverts if the `staker` is also an operator, since operators are not allowed to undelegate from themselves.
      * @dev Reverts if the caller is not the staker, nor the operator who the staker is delegated to, nor the operator's specified "delegationApprover"
-     * @dev Reverts if the `staker` is already undelegated.
+     * @dev Reverts if the `staker` is not delegated to an operator
      */
     function undelegate(
         address staker
