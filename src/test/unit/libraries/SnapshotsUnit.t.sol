20beb042b871be486c338fef42282e652875599f
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-01-06 18:59:52 -0500

    test: `Snapshots` lib (#1002)

---
+++ src/test/unit/libraries/SnapshotsUnit.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "forge-std/Test.sol";
+import "src/contracts/libraries/Snapshots.sol";
+
+contract SnapshotsUnitTests is Test {
+    using Snapshots for Snapshots.DefaultWadHistory;
+
+    Snapshots.DefaultWadHistory history;
+
+    function test_Revert_InvalidSnapshotOrdering(uint256 r) public {
+        uint32 key = uint32(bound(r, 1, type(uint32).max));
+        uint32 smallerKey = uint32(bound(r, 0, key - 1));
+
+        history.push(key, 1);
+
+        vm.expectRevert(Snapshots.InvalidSnapshotOrdering.selector);
+        history.push(smallerKey, 2);
+    }
+
+    function test_Push_Correctness(uint256 r) public {
+        uint32 key = uint32(bound(r, 0, type(uint32).max));
+        uint64 value = uint32(bound(r, 0, type(uint64).max));
+
+        history.push(key, value);
+
+        assertEq(history.upperLookup(key), value);
+        assertEq(history.latest(), value);
+        assertEq(history.length(), 1);
+    }
+
+    function test_UpperLookup_InitiallyWad(uint32 r) public {
+        assertEq(history.upperLookup(r), 1e18);
+    }
+
+    function test_Latest_InitiallyWad() public {
+        assertEq(history.latest(), 1e18);
+    }
+
+    function test_Length_InitiallyZero() public {
+        assertEq(history.length(), 0);
+    }
+}
