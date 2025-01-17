d8f77d47ec6fd09942534e88e9d3673138993131
Bowen Li <bowenli86@gmail.com>
2025-01-17 11:04:57 -0800

    fix: remove token param from Deposit event and related APIs (#1013)
    * fix: remove token param from Deposit event and related APIs
    
    * fix: forge fmt
    
    * fix: rebase
    
    * fix: update EigenPodManager and test
    
    * fix: update tests
    
    * fix: update eigenpodmanager tests
    
    * fix: update StrategyManagerMock
    
    * feat: add bindings
    
    * fix: update docs

--- src/contracts/core/StrategyManager.sol
+++ src/contracts/core/StrategyManager.sol
@@ -124,10 +124,9 @@ contract StrategyManager is
     function addShares(
         address staker,
         IStrategy strategy,
-        IERC20 token,
         uint256 shares
     ) external onlyDelegationManager returns (uint256, uint256) {
-        return _addShares(staker, token, strategy, shares);
+        return _addShares(staker, strategy, shares);
     }
 
     /// @inheritdoc IShareManager
@@ -197,19 +196,13 @@ contract StrategyManager is
     /**
      * @notice This function adds `shares` for a given `strategy` to the `staker` and runs through the necessary update logic.
      * @param staker The address to add shares to
-     * @param token The token that is being deposited (used for indexing)
      * @param strategy The Strategy in which the `staker` is receiving shares
      * @param shares The amount of shares to grant to the `staker`
      * @dev In particular, this function calls `delegation.increaseDelegatedShares(staker, strategy, shares)` to ensure that all
      * delegated shares are tracked, increases the stored share amount in `stakerDepositShares[staker][strategy]`, and adds `strategy`
      * to the `staker`'s list of strategies, if it is not in the list already.
      */
-    function _addShares(
-        address staker,
-        IERC20 token,
-        IStrategy strategy,
-        uint256 shares
-    ) internal returns (uint256, uint256) {
+    function _addShares(address staker, IStrategy strategy, uint256 shares) internal returns (uint256, uint256) {
         // sanity checks on inputs
         require(staker != address(0), StakerAddressZero());
         require(shares != 0, SharesAmountZero());
@@ -225,7 +218,7 @@ contract StrategyManager is
         // add the returned depositedShares to their existing shares for this strategy
         stakerDepositShares[staker][strategy] = prevDepositShares + shares;
 
-        emit Deposit(staker, token, strategy, shares);
+        emit Deposit(staker, strategy, shares);
         return (prevDepositShares, shares);
     }
 
@@ -251,7 +244,7 @@ contract StrategyManager is
         shares = strategy.deposit(token, amount);
 
         // add the returned shares to the staker's existing shares for this strategy
-        (uint256 prevDepositShares, uint256 addedShares) = _addShares(staker, token, strategy, shares);
+        (uint256 prevDepositShares, uint256 addedShares) = _addShares(staker, strategy, shares);
 
         // Increase shares delegated to operator
         delegation.increaseDelegatedShares({
