bfa0c7f4c1f9bbff4ed0b9254ee3cb6c27b45e94
Michael <michael@Michaels-MacBook-Pro.local>
2025-02-05 13:00:52 -0500

    chore: integration prefix

--- src/test/integration/tests/Slashed_Eigenpod.t.sol
+++ src/test/integration/tests/Slashed_Eigenpod.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.27;
 
 import "src/test/integration/IntegrationChecks.t.sol";
 
-contract SlashedEigenpod is IntegrationCheckUtils {
+contract Integration_SlashedEigenpod is IntegrationCheckUtils {
     using ArrayLib for *;
     
     AVS avs;
