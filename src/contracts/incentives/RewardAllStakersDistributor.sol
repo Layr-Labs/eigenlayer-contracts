d7c521f4dba59cb0e0e71f15c1c7b36d16625dcb
MinisculeTarantula <jeff@eigenfoundation.org>
2025-02-12 10:19:05 -0700

    feat: support for multi-TIMESCALE distributions
    adds a bit of complexity but deals with cases where a TIMESCALE is 'missed'
    
    also provides a route for having the initial distribution stretch into the past / start at a definite time in the future, if so desired

--- src/contracts/incentives/RewardAllStakersDistributor.sol
+++ src/contracts/incentives/RewardAllStakersDistributor.sol
@@ -43,18 +43,36 @@ contract RewardAllStakersDistributor is IIncentivesDistributor {
     IERC20 public immutable EIGEN;
     uint256 public immutable vectorIndex;
 
+    /**
+     * @notice The last time that `distributeIncentives` was called successfully.
+     * @dev The initial value of this variable is set via constructor input.
+     */
+    uint256 public lastDistributionTimestamp;
+
+    /**
+     * @dev Note that lastDistributionTimestamp can be initialized to either a past or future timestamp, depending on the desired behavior
+     * (an initial distribution reaching into the past or an initial distribution starting in the future, respectively).
+     * It is initialized to the current time if the `init_lastDistributionTimestamp` input is set to zero. Due to timestamp treatment
+     * in the `distributeIncentives` function, what ultimately matters for initialization is `init_lastDistributionTimestamp / TIMESCALE`.
+     */
     constructor(
         IProgrammaticIncentivesConfig _programmaticIncentivesConfig,
         address _rewardsCoordinator,
         IERC20 _bEIGEN,
         IERC20 _EIGEN,
-        uint256 _vectorIndex
+        uint256 _vectorIndex,
+        uint256 init_lastDistributionTimestamp
     ) {
         programmaticIncentivesConfig = _programmaticIncentivesConfig;
         rewardsCoordinator = _rewardsCoordinator;
         bEIGEN = _bEIGEN;
         EIGEN = _EIGEN;
         vectorIndex = _vectorIndex;
+        if (init_lastDistributionTimestamp == 0) {
+            lastDistributionTimestamp = block.timestamp;            
+        } else {
+            lastDistributionTimestamp = init_lastDistributionTimestamp;
+        }
     }
 
     function distributeIncentives() external {
@@ -64,29 +82,37 @@ contract RewardAllStakersDistributor is IIncentivesDistributor {
         // 1) check how many tokens were minted (also accounts for transfers in)
         uint256 tokenAmount = bEIGEN.balanceOf(address(this));
 
-        // 2) approve the bEIGEN token for transfer so it can be wrapped
-        bEIGEN.safeApprove(address(EIGEN), tokenAmount);
-
-        // 3) wrap the bEIGEN token to receive EIGEN
-        IEigen(address(EIGEN)).wrap(tokenAmount);
-
-        // 4) Set the proper allowance on the coordinator
-        EIGEN.safeApprove(address(rewardsCoordinator), tokenAmount);
-
-        // 5) Call the reward coordinator's ForAll API
-        IRewardsCoordinator_VectorModification.RewardsSubmission[] memory rewardsSubmission =
-            new IRewardsCoordinator_VectorModification.RewardsSubmission[](1);
-        rewardsSubmission[0] = IRewardsCoordinator_VectorModification.RewardsSubmission({
-            strategiesAndMultipliers: programmaticIncentivesConfig.vector(vectorIndex),
-            token: EIGEN,
-            amount: tokenAmount,
+        if (tokenAmount > 0) {
+            uint256 timescalesElapsed = (block.timestamp / TIMESCALE) - (lastDistributionTimestamp / TIMESCALE);
+            uint32 duration = uint32(timescalesElapsed * TIMESCALE);
+            lastDistributionTimestamp = block.timestamp;
             // round up to timescale (i.e. start of *next* 'TIMESCALE' interval, in UTC time)
-            startTimestamp: uint32(((block.timestamp / TIMESCALE) + 1) * TIMESCALE),
-            duration: uint32(TIMESCALE)
-        });
-        IRewardsCoordinator_VectorModification(rewardsCoordinator).createRewardsForAllEarners(rewardsSubmission);
+            // start earlier in time if multiple TIMESCALE borders have been crossed since lastDistributionTimestamp
+            uint32 startTimestamp = uint32(((block.timestamp / TIMESCALE) + 2 - timescalesElapsed) * TIMESCALE);
 
-        emit IncentivesDistributed(tokenAmount);
+            // 2) approve the bEIGEN token for transfer so it can be wrapped
+            bEIGEN.safeApprove(address(EIGEN), tokenAmount);
+
+            // 3) wrap the bEIGEN token to receive EIGEN
+            IEigen(address(EIGEN)).wrap(tokenAmount);
+
+            // 4) Set the proper allowance on the coordinator
+            EIGEN.safeApprove(address(rewardsCoordinator), tokenAmount);
+
+            // 5) Call the reward coordinator's ForAll API
+            IRewardsCoordinator_VectorModification.RewardsSubmission[] memory rewardsSubmission =
+                new IRewardsCoordinator_VectorModification.RewardsSubmission[](1);
+            rewardsSubmission[0] = IRewardsCoordinator_VectorModification.RewardsSubmission({
+                strategiesAndMultipliers: programmaticIncentivesConfig.vector(vectorIndex),
+                token: EIGEN,
+                amount: tokenAmount,
+                startTimestamp: startTimestamp,
+                duration: uint32(duration)
+            });
+            IRewardsCoordinator_VectorModification(rewardsCoordinator).createRewardsForAllEarners(rewardsSubmission);
+
+            emit IncentivesDistributed(tokenAmount);
+        }
     }
 }
 
