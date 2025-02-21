50c04458df31d830f615c1be01c4edb706889d92
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-02-21 09:52:46 -0500

    test: add delegate deposit queue complete before upgrade as shares

--- src/test/integration/tests/upgrade/Complete_PreSlashing_Withdrawal.t.sol
+++ src/test/integration/tests/upgrade/Complete_PreSlashing_Withdrawal.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.27;
 import "src/test/integration/UpgradeTest.t.sol";
 
 contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
-
     function testFuzz_deposit_queue_upgrade_completeAsShares(uint24 _random) public rand(_random) {
         /// Pre-upgrade:
         /// 1. Create staker with some assets
@@ -187,6 +186,35 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
         for (uint i = 0; i < withdrawals.length; i++) {
             IERC20[] memory tokens = staker.completeWithdrawalAsTokens(withdrawals[i]);
             check_Withdrawal_AsTokens_State(staker, operator, withdrawals[i], strategies, shares, tokens, expectedTokens);
+        }
+    }
+
+    function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_asShares(uint24 _random) public rand(_random) {
+        /// Pre-upgrade:
+        /// 1. Create staker and operator with some asset amounts
+        /// 2. Staker delegates to operator
+        /// 3. Staker deposits into EigenLayer
+        /// 4. Staker queues a withdrawal
+        (User staker, IStrategy[] memory strategies, uint256[] memory tokenBalances) = _newRandomStaker();
+        (User operator,,) = _newRandomOperator();
+
+        staker.delegateTo(operator);
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        uint256[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
+        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, shares);
+
+        _rollBlocksForCompleteWithdrawals(withdrawals);
+
+        // We must roll forward by the delay twice since pre-upgrade delay is half as long as post-upgrade delay.
+        rollForward(delegationManager.minWithdrawalDelayBlocks() + 1);
+
+        /// Upgrade to slashing contracts
+        _upgradeEigenLayerContracts();
+
+        // Complete pre-slashing withdrawals as shares
+        for (uint256 i = 0; i < withdrawals.length; i++) {
+            staker.completeWithdrawalAsShares(withdrawals[i]);
+            check_Withdrawal_AsShares_State(staker, operator, withdrawals[i], strategies, shares);
         }
     }
 }
