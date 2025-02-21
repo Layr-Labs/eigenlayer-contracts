3983c56702ffdf0f4d82f766871d56e8eb9044f7
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-02-21 10:14:15 -0500

    test: add partial withdrawal tests for pre-upgrade completion and reorder test functions

--- src/test/integration/tests/upgrade/Complete_PreSlashing_Withdrawal.t.sol
+++ src/test/integration/tests/upgrade/Complete_PreSlashing_Withdrawal.t.sol
@@ -27,6 +27,30 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
         }
     }
 
+    function testFuzz_deposit_queue_upgrade_completeAsTokens(uint24 _random) public rand(_random) {
+        /// Pre-upgrade:
+        /// 1. Create staker with some assets
+        /// 2. Staker deposits into EigenLayer
+        /// 3. Staker queues a withdrawal
+        (User staker, IStrategy[] memory strategies, uint[] memory tokenBalances) = _newRandomStaker();
+        User operator = User(payable(0));
+
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        uint[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
+        uint[] memory expectedTokens = _calculateExpectedTokens(strategies, shares);
+        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, shares);
+    
+        /// Upgrade to slashing contracts
+        _upgradeEigenLayerContracts();
+    
+        // Complete pre-slashing withdrawals as tokens
+        _rollBlocksForCompleteWithdrawals(withdrawals);
+        for (uint i = 0; i < withdrawals.length; i++) {
+            IERC20[] memory tokens = staker.completeWithdrawalAsTokens(withdrawals[i]);
+            check_Withdrawal_AsTokens_State(staker, operator, withdrawals[i], strategies, shares, tokens, expectedTokens);
+        }
+    }
+
     function testFuzz_delegate_deposit_queue_upgrade_completeAsShares(uint24 _random) public rand(_random) {
         /// Pre-upgrade:
         /// 1. Create staker and operator with some asset amounts
@@ -49,30 +73,6 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
         for (uint i = 0; i < withdrawals.length; i++) {
             staker.completeWithdrawalAsShares(withdrawals[i]);
             check_Withdrawal_AsShares_State(staker, operator, withdrawals[i], strategies, shares);
-        }
-    }
-
-    function testFuzz_deposit_queue_upgrade_completeAsTokens(uint24 _random) public rand(_random) {
-        /// Pre-upgrade:
-        /// 1. Create staker with some assets
-        /// 2. Staker deposits into EigenLayer
-        /// 3. Staker queues a withdrawal
-        (User staker, IStrategy[] memory strategies, uint[] memory tokenBalances) = _newRandomStaker();
-        User operator = User(payable(0));
-
-        staker.depositIntoEigenlayer(strategies, tokenBalances);
-        uint[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
-        uint[] memory expectedTokens = _calculateExpectedTokens(strategies, shares);
-        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, shares);
-    
-        /// Upgrade to slashing contracts
-        _upgradeEigenLayerContracts();
-    
-        // Complete pre-slashing withdrawals as tokens
-        _rollBlocksForCompleteWithdrawals(withdrawals);
-        for (uint i = 0; i < withdrawals.length; i++) {
-            IERC20[] memory tokens = staker.completeWithdrawalAsTokens(withdrawals[i]);
-            check_Withdrawal_AsTokens_State(staker, operator, withdrawals[i], strategies, shares, tokens, expectedTokens);
         }
     }
 
@@ -159,6 +159,35 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
         }
     }
 
+    function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_asShares(uint24 _random) public rand(_random) {
+        /// Pre-upgrade:
+        /// 1. Create staker and operator with some asset amounts
+        /// 2. Staker delegates to operator
+        /// 3. Staker deposits into EigenLayer
+        /// 4. Staker queues a withdrawal
+        (User staker, IStrategy[] memory strategies, uint256[] memory tokenBalances) = _newRandomStaker();
+        (User operator,,) = _newRandomOperator();
+
+        staker.delegateTo(operator);
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        uint256[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
+        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, shares);
+
+        _rollBlocksForCompleteWithdrawals(withdrawals);
+
+        // We must roll forward by the delay twice since pre-upgrade delay is half as long as post-upgrade delay.
+        rollForward(delegationManager.minWithdrawalDelayBlocks() + 1);
+
+        /// Upgrade to slashing contracts
+        _upgradeEigenLayerContracts();
+
+        // Complete pre-slashing withdrawals as shares
+        for (uint256 i = 0; i < withdrawals.length; i++) {
+            staker.completeWithdrawalAsShares(withdrawals[i]);
+            check_Withdrawal_AsShares_State(staker, operator, withdrawals[i], strategies, shares);
+        }
+    }
+
     function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_asTokens(uint24 _random) public rand(_random) {
         /// Pre-upgrade:
         /// 1. Create staker and operator with some asset amounts
@@ -189,19 +218,24 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
         }
     }
 
-    function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_asShares(uint24 _random) public rand(_random) {
+    function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_partial_asShares(uint24 _random) public rand(_random) {
         /// Pre-upgrade:
         /// 1. Create staker and operator with some asset amounts
         /// 2. Staker delegates to operator
         /// 3. Staker deposits into EigenLayer
-        /// 4. Staker queues a withdrawal
+        /// 4. Staker queues a partial withdrawal
         (User staker, IStrategy[] memory strategies, uint256[] memory tokenBalances) = _newRandomStaker();
         (User operator,,) = _newRandomOperator();
 
         staker.delegateTo(operator);
         staker.depositIntoEigenlayer(strategies, tokenBalances);
         uint256[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
-        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, shares);
+        
+        uint256[] memory partialShares = new uint256[](shares.length);
+        for (uint256 i = 0; i < shares.length; i++) {
+            partialShares[i] = shares[i] / 2;
+        }
+        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, partialShares);
 
         _rollBlocksForCompleteWithdrawals(withdrawals);
 
@@ -214,7 +248,42 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
         // Complete pre-slashing withdrawals as shares
         for (uint256 i = 0; i < withdrawals.length; i++) {
             staker.completeWithdrawalAsShares(withdrawals[i]);
-            check_Withdrawal_AsShares_State(staker, operator, withdrawals[i], strategies, shares);
+            check_Withdrawal_AsShares_State(staker, operator, withdrawals[i], strategies, partialShares);
+        }
+    }
+
+    function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_partial_asTokens(uint24 _random) public rand(_random) {
+        /// Pre-upgrade:
+        /// 1. Create staker and operator with some asset amounts
+        /// 2. Staker delegates to operator
+        /// 3. Staker deposits into EigenLayer
+        /// 4. Staker queues a partial withdrawal
+        (User staker, IStrategy[] memory strategies, uint[] memory tokenBalances) = _newRandomStaker();
+        (User operator, ,) = _newRandomOperator();
+
+        staker.delegateTo(operator);
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        uint[] memory shares = _calculateExpectedShares(strategies, tokenBalances);
+        
+        uint[] memory partialShares = new uint[](shares.length);
+        for (uint i = 0; i < shares.length; i++) {
+            partialShares[i] = shares[i] / 2;
+        }
+        uint[] memory expectedTokens = _calculateExpectedTokens(strategies, partialShares);
+        Withdrawal[] memory withdrawals = staker.queueWithdrawals(strategies, partialShares);
+
+        _rollBlocksForCompleteWithdrawals(withdrawals);
+
+        // We must roll forward by the delay twice since pre-upgrade delay is half as long as post-upgrade delay.
+        rollForward(delegationManager.minWithdrawalDelayBlocks() + 1);
+
+        /// Upgrade to slashing contracts
+        _upgradeEigenLayerContracts();
+
+        // Complete pre-slashing withdrawals as tokens
+        for (uint i = 0; i < withdrawals.length; i++) {
+            IERC20[] memory tokens = staker.completeWithdrawalAsTokens(withdrawals[i]);
+            check_Withdrawal_AsTokens_State(staker, operator, withdrawals[i], strategies, partialShares, tokens, expectedTokens);
         }
     }
 }
