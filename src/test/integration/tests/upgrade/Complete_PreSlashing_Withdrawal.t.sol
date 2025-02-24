0c3d923e945ad71062f09048318301fa7c7e8b5a
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-02-24 11:18:24 -0500

    test: upgrade allocate correct slashable stake

--- src/test/integration/tests/upgrade/Complete_PreSlashing_Withdrawal.t.sol
+++ src/test/integration/tests/upgrade/Complete_PreSlashing_Withdrawal.t.sol
@@ -160,11 +160,6 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
     }
 
     function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_asShares(uint24 _random) public rand(_random) {
-        /// Pre-upgrade:
-        /// 1. Create staker and operator with some asset amounts
-        /// 2. Staker delegates to operator
-        /// 3. Staker deposits into EigenLayer
-        /// 4. Staker queues a withdrawal
         (User staker, IStrategy[] memory strategies, uint256[] memory tokenBalances) = _newRandomStaker();
         (User operator,,) = _newRandomOperator();
 
@@ -189,11 +184,6 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
     }
 
     function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_asTokens(uint24 _random) public rand(_random) {
-        /// Pre-upgrade:
-        /// 1. Create staker and operator with some asset amounts
-        /// 2. Staker delegates to operator
-        /// 3. Staker deposits into EigenLayer
-        /// 4. Staker queues a withdrawal
         (User staker, IStrategy[] memory strategies, uint[] memory tokenBalances) = _newRandomStaker();
         (User operator, ,) = _newRandomOperator();
 
@@ -219,11 +209,6 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
     }
 
     function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_partial_asShares(uint24 _random) public rand(_random) {
-        /// Pre-upgrade:
-        /// 1. Create staker and operator with some asset amounts
-        /// 2. Staker delegates to operator
-        /// 3. Staker deposits into EigenLayer
-        /// 4. Staker queues a partial withdrawal
         (User staker, IStrategy[] memory strategies, uint256[] memory tokenBalances) = _newRandomStaker();
         (User operator,,) = _newRandomOperator();
 
@@ -253,11 +238,6 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
     }
 
     function testFuzz_delegate_deposit_queue_completeBeforeUpgrade_partial_asTokens(uint24 _random) public rand(_random) {
-        /// Pre-upgrade:
-        /// 1. Create staker and operator with some asset amounts
-        /// 2. Staker delegates to operator
-        /// 3. Staker deposits into EigenLayer
-        /// 4. Staker queues a partial withdrawal
         (User staker, IStrategy[] memory strategies, uint[] memory tokenBalances) = _newRandomStaker();
         (User operator, ,) = _newRandomOperator();
 
@@ -285,5 +265,38 @@ contract Integration_Upgrade_Complete_PreSlashing_Withdrawal is UpgradeTest {
             IERC20[] memory tokens = staker.completeWithdrawalAsTokens(withdrawals[i]);
             check_Withdrawal_AsTokens_State(staker, operator, withdrawals[i], strategies, partialShares, tokens, expectedTokens);
         }
+    }
+
+    function testFuzz_upgrade_allocate_correctSlashableStake(uint24 _random) public rand(_random) {
+        (User staker, IStrategy[] memory strategies, uint256[] memory tokenBalances) = _newRandomStaker();
+        (User operator,,) = _newRandomOperator();
+
+        // Pre-upgrade:
+        // 1. Create staker and operator with assets, then deposit into EigenLayer
+        // 2. Delegate to operator
+        staker.depositIntoEigenlayer(strategies, tokenBalances);
+        staker.delegateTo(operator);
+
+        // Upgrade to slashing release
+        _upgradeEigenLayerContracts();
+        (AVS avs,) = _newRandomAVS();
+
+        // 3. Set allocation delay for operator
+        operator.setAllocationDelay(1);
+        rollForward({blocks: ALLOCATION_CONFIGURATION_DELAY + 1});
+
+        // 4. Create an operator set and register an operator.
+        OperatorSet memory operatorSet = avs.createOperatorSet(strategies);
+        operator.registerForOperatorSet(operatorSet);
+        check_Registration_State_NoAllocation(operator, operatorSet, allStrats);
+
+        // 5. Allocate to operator set.
+        AllocateParams memory allocateParams = AllocateParams({
+            operatorSet: operatorSet,
+            strategies: strategies,
+            newMagnitudes: _randMagnitudes({sum: 1 ether, len: strategies.length})
+        });
+        operator.modifyAllocations(allocateParams);
+        check_IncrAlloc_State_Slashable(operator, allocateParams);
     }
 }
