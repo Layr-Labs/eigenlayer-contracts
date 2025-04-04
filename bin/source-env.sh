c7e9e6ad7b9a53e4ce2819cc90e0f74b8c953b03
Bowen Li <bowenli86@gmail.com>
2025-04-04 09:24:35 -0700

    chore: remove goerli from all codebase (#1304)
    **Motivation:**
    
    goerli has long been gone, we should remove goerli from all codebase
    
    **Modifications:**
    
    remove goerli from all codebase
    
    **Result:**
    
    remove goerli from all codebase

--- bin/source-env.sh
+++ bin/source-env.sh
@@ -2,22 +2,17 @@
 
 # Check for arguments
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 [goerli|local]"
+    echo "Usage: $0 [local]"
     return 1
 fi
 
 case $1 in
-    goerli)
-        CHAIN_ID=5
-        EXECUTOR_MULTISIG="0x3d9C2c2B40d890ad53E27947402e977155CD2808"
-        FOUNDRY_FUZZ_RUNS=1
-        ;;
     local)
         CHAIN_ID=31337
         FOUNDRY_FUZZ_RUNS=256
         ;;
     *)
-        echo "Invalid argument. Usage: $0 [goerli|local]"
+        echo "Invalid argument. Usage: $0 [local]"
         return 1
         ;;
 esac
