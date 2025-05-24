c3be36a655cf58d72475cd351941878a4abfe081
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-05-24 15:17:20 -0400

    chore: fmt

--- src/contracts/core/DelegationManager.sol
+++ src/contracts/core/DelegationManager.sol
@@ -317,7 +317,9 @@ contract DelegationManager is
         // Emit event for operator shares being slashed
         emit OperatorSharesSlashed(operator, strategy, totalDepositSharesToBurn);
 
-        _getShareManager(strategy).increaseBurnableShares(operatorSet, slashId, strategy, totalDepositSharesToBurn);
+        _getShareManager(strategy).increaseBurnOrRedistributableShares(
+            operatorSet, slashId, strategy, totalDepositSharesToBurn
+        );
 
         return totalDepositSharesToBurn;
     }
