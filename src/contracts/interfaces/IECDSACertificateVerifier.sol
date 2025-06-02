ebb3ba6204f5b455f2072618cb1156c9af98c513
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-02 11:50:26 -0400

    feat: multi chain interfaces (#1423)
    **Motivation:**
    
    Add all multi chain interfaces for easier development. 
    
    **Modifications:**
    
    Adds the following:
    - `IBN254CertificateVerifier` (implements `IBaseCertificateVerifier`)
    - `IECDSACertificateVerifier` (implements `IBaseCertificateVerifier`)
    - `IBN254TableCalculator`
    - `IECDSATableCalculator`
    - `IOperatorTableUpdater`
    - `ICrossChainRegistry`
    
    **Result:**
    
    Multichain interfaces. Pending adding of `OperatorTableCalculator`

---
+++ src/contracts/interfaces/IECDSACertificateVerifier.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "src/contracts/libraries/OperatorSetLib.sol";
+import "src/contracts/interfaces/ICrossChainRegistry.sol";
+import "src/contracts/interfaces/IBaseCertificateVerifier.sol";
+import "src/contracts/interfaces/IECDSATableCalculator.sol";
+
+interface IECDSACertificateVerifierTypes is IECDSATableCalculatorTypes {
+    /**
+     * @notice A ECDSA Certificate
+     * @param referenceTimestamp the timestamp at which the certificate was created
+     * @param messageHash the hash of the message that was signed by operators
+     * @param signature the concatenated signature of each signing operator
+     */
+    struct ECDSACertificate {
+        uint32 referenceTimestamp;
+        bytes32 messageHash;
+        bytes sig;
+    }
+}
+
+interface IECDSACertificateVerifierEvents is IECDSACertificateVerifierTypes {
+    /// @notice Emitted when an ECDSA table is updated
+    event TableUpdated(OperatorSet operatorSet, uint32 referenceTimestamp, ECDSAOperatorInfo[] operatorInfos);
+}
+
+/// @notice An interface for verifying ECDSA certificates
+/// @notice This implements a base interface that all curve certificate verifiers (eg. BN254, ECDSA) must implement
+interface IECDSACertificateVerifier is IECDSACertificateVerifierEvents, IBaseCertificateVerifier {
+    /**
+     * @notice updates the operator table
+     * @param operatorSet the operatorSet to update the operator table for
+     * @param referenceTimestamp the timestamp at which the operatorInfos were sourced
+     * @param operatorInfos the operatorInfos to update the operator table with
+     * @param operatorSetConfig the configuration of the operatorSet
+     * @dev only callable by the operatorTableUpdater for the given operatorSet
+     * @dev The `referenceTimestamp` must be greater than the latest reference timestamp for the given operatorSet
+     */
+    function updateOperatorTable(
+        OperatorSet calldata operatorSet,
+        uint32 referenceTimestamp,
+        ECDSAOperatorInfo[] calldata operatorInfos,
+        OperatorSetConfig calldata operatorSetConfig
+    ) external;
+
+    /**
+     * @notice verifies a certificate
+     * @param cert a certificate
+     * @return signedStakes amount of stake that signed the certificate for each stake
+     * type
+     */
+    function verifyCertificate(
+        OperatorSet calldata operatorSet,
+        ECDSACertificate memory cert
+    ) external returns (uint256[] memory signedStakes);
+
+    /**
+     * @notice verifies a certificate and makes sure that the signed stakes meet
+     * provided portions of the total stake on the AVS
+     * @param cert a certificate
+     * @param totalStakeProportionThresholds the proportion of total stake that
+     * the signed stake of the certificate should meet
+     * @return Whether or not the certificate is valid and meets thresholds
+     */
+    function verifyCertificateProportion(
+        OperatorSet calldata operatorSet,
+        ECDSACertificate memory cert,
+        uint16[] memory totalStakeProportionThresholds
+    ) external returns (bool);
+
+    /**
+     * @notice verifies a certificate and makes sure that the signed stakes meet
+     * provided portions of the total stake on the AVS
+     * @param cert a certificate
+     * @param totalStakeNominalThresholds the proportion of total stake that
+     * the signed stake of the certificate should meet
+     * @return Whether or not the certificate is valid and meets thresholds
+     */
+    function verifyCertificateNominal(
+        OperatorSet calldata operatorSet,
+        ECDSACertificate memory cert,
+        uint256[] memory totalStakeNominalThresholds
+    ) external returns (bool);
+}
