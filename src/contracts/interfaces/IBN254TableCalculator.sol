fa58d89fe211994a6014c7ec8257689b8daa2350
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-02 11:50:26 -0400

    feat: multi chain interfaces (#1423)
    **Motivation:**
    
    Add all multi chain interfaces for easier development. 
    
    **Modifications:**
    
    Adds the following:
    - `IBN254CertificateVerifier` (implements `IBaseCertificateVerifier`)
    - `IECDSACertificateVerifier` (implements `IBaseCertificateVerifier`)
    - `IBN254TableCalculator`
    - `IECDSATableCalculator`
    - `IOperatorTableUpdater`
    - `ICrossChainRegistry`
    
    **Result:**
    
    Multichain interfaces. Pending adding of `OperatorTableCalculator`

---
+++ src/contracts/interfaces/IBN254TableCalculator.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "src/contracts/libraries/BN254.sol";
+import "src/contracts/libraries/OperatorSetLib.sol";
+
+interface IBN254TableCalculatorTypes {
+    /**
+     * @notice A struct that contains information about a single operator
+     * @param pubkey The G1 public key of the operator.
+     * @param weights The weights of the operator for a single operatorSet.
+     * @dev The `weights` array can be defined as a list of arbitrary groupings. For example,
+     * it can be [slashable_stake, delegated_stake, strategy_i_stake, ...]
+     */
+    struct BN254OperatorInfo {
+        BN254.G1Point pubkey;
+        uint256[] weights;
+    }
+
+    /**
+     * @notice A struct that contains information about all operators for a given operatorSet
+     * @param operatorInfoTreeRoot The root of the operatorInfo tree.
+     * @param numOperators The number of operators in the operatorSet.
+     * @param aggregatePubkey The aggregate G1 public key of the operators in the operatorSet.
+     * @param totalWeights The total weights of the operators in the operatorSet.
+     *
+     * @dev The operatorInfoTreeRoot is the root of a merkle tree that contains the operatorInfos for each operator in the operatorSet.
+     * It is calculated in this function and used by the `IBN254CertificateVerifier` to verify stakes against the non-signing operators
+     *
+     * @dev Retrieval of the `aggregatePubKey` depends on maintaining a key registry contract, see `BLSAPKRegistry` for an example implementation.
+     *
+     * @dev The `totalWeights` array should be the same length as each individual `weights` array in `operatorInfos`.
+     */
+    struct BN254OperatorSetInfo {
+        bytes32 operatorInfoTreeRoot;
+        uint256 numOperators;
+        BN254.G1Point aggregatePubkey;
+        uint256[] totalWeights;
+    }
+}
+
+interface IBN254TableCalculator is IBN254TableCalculatorTypes {
+    /**
+     * @notice calculates the operatorInfos for a given operatorSet
+     * @param operatorSet the operatorSet to calculate the operator table for
+     * @return operatorSetInfo the operatorSetInfo for the given operatorSet
+     * @dev The output of this function is converted to bytes via the `calculateOperatorTableBytes` function
+     */
+    function calculateOperatorTable(
+        OperatorSet calldata operatorSet
+    ) external view returns (BN254OperatorSetInfo memory operatorSetInfo);
+
+    /**
+     * @notice calculates the operatorInfos for a given operatorSet
+     * @param operatorSet the operatorSet to calculate the operator table for
+     * @return operatorTableBytes the operatorTableBytes for the given operatorSet
+     */
+    function calculateOperatorTableBytes(
+        OperatorSet calldata operatorSet
+    ) external view returns (bytes memory operatorTableBytes);
+
+    /**
+     * @notice Get the operatorInfos for a given operatorSet
+     * @param operatorSet the operatorSet to get the operatorInfos for
+     * @return operatorInfos the operatorInfos for the given operatorSet
+     */
+    function getOperatorInfos(
+        OperatorSet calldata operatorSet
+    ) external view returns (BN254OperatorInfo[] memory operatorInfos);
+}
---
+++ src/contracts/interfaces/ICrossChainRegistry.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "src/contracts/libraries/OperatorSetLib.sol";
+
+interface IOperatorTableCalculator {
+// TODO: implement, stub for now
+}
+
+interface ICrossChainRegistryErrors {
+    /// @notice Thrown when the chainId is invalid
+    error InvalidChainId();
+}
+
+interface ICrossChainRegistryTypes {
+    /**
+     * @notice A per-operatorSet configuration struct that is transported from the CrossChainRegistry on L1.
+     * @param owner the permissioned owner of the OperatorSet on L2 that can call the CertificateVerifier specific setters
+     * @param maxStalenessPeriod the maximum staleness period of the operatorSet
+     */
+    struct OperatorSetConfig {
+        address owner;
+        uint32 maxStalenessPeriod;
+    }
+}
+
+interface ICrossChainRegistryEvents {
+    /// @notice Emitted when a generation reservation is made
+    event GenerationReservationMade(OperatorSet operatorSet, IOperatorTableCalculator operatorTableCalculator);
+
+    /// @notice Emitted when a generation reservation is removed
+    event GenerationReservationRemoved(OperatorSet operatorSet, IOperatorTableCalculator operatorTableCalculator);
+
+    /// @notice Emitted when a transport destination is added
+    event TransportDestinationAdded(OperatorSet operatorSet, uint32 chainID);
+
+    /// @notice Emitted when a transport destination is removed
+    event TransportDestinationRemoved(OperatorSet operatorSet, uint32 chainID);
+
+    /// @notice Emitted when a chainID is added to the whitelist
+    event ChainIDAddedToWhitelist(uint32 chainID);
+
+    /// @notice Emitted when a chainID is removed from the whitelist
+    event ChainIDRemovedFromWhitelist(uint32 chainID);
+}
+
+interface ICrossChainRegistry is ICrossChainRegistryErrors, ICrossChainRegistryTypes, ICrossChainRegistryEvents {
+    /**
+     * @notice Initiates a generation reservation
+     * @param operatorSet the operatorSet to make a reservation for
+     * @param operatorTableCalculator the address of the operatorTableCalculator
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     */
+    function requestGenerationReservation(OperatorSet calldata operatorSet, address operatorTableCalculator) external;
+
+    /**
+     * @notice Removes a generation reservation for a given operatorSet
+     * @param operatorSet the operatorSet to remove
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     */
+    function removeGenerationReservation(
+        OperatorSet calldata operatorSet
+    ) external;
+
+    /**
+     * @notice Adds a destination chain to transport to
+     * @param chainID to add transport to
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     */
+    function addTransportDestination(OperatorSet calldata operatorSet, uint32 chainID) external;
+
+    /**
+     * @notice Removes a destination chain to transport to
+     * @param chainID to remove transport to
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     */
+    function removeTransportDestination(OperatorSet calldata operatorSet, uint32 chainID) external;
+
+    /**
+     * @notice Sets the operatorTableCalculator for the operatorSet
+     * @param operatorSet the operatorSet whose operatorTableCalculator is desired to be set
+     * @param calculator the contract to call to calculate the operator table
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     * @dev operatorSet must have an active reservation
+     */
+    function setOperatorTableCalculator(
+        OperatorSet calldata operatorSet,
+        IOperatorTableCalculator calculator
+    ) external;
+
+    /**
+     * @notice Adds a chainID to the whitelist of chainIDs that can be transported to
+     * @param chainID the chainID to add to the whitelist
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     */
+    function addChainIDToWhitelist(
+        uint32 chainID
+    ) external;
+
+    /**
+     * @notice Removes a chainID from the whitelist of chainIDs that can be transported to
+     * @param chainID the chainID to remove from the whitelist
+     * @dev msg.sender must be UAM permissioned for operatorSet.avs
+     */
+    function removeChainIDFromWhitelist(
+        uint32 chainID
+    ) external;
+
+    /**
+     * @notice Gets the operatorTableCalculator for a given operatorSet
+     * @param operatorSet the operatorSet to get the operatorTableCalculator for
+     * @return The operatorTableCalculator for the given operatorSet
+     */
+    function getOperatorTableCalculator(
+        OperatorSet calldata operatorSet
+    ) external returns (IOperatorTableCalculator);
+
+    /**
+     * @notice Gets the active generation reservations
+     * @return An array of operatorSets with active generationReservations
+     * @return An array of the corresponding operatorTableCalculators
+     */
+    function getActiveGenerationReservations()
+        external
+        returns (OperatorSet[] memory, IOperatorTableCalculator[] memory);
+}
