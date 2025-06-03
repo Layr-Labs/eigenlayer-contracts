1811bde052593c67ce2cb1a52c75a73e407efb4e
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-03 17:16:11 -0400

    chore: bindings + interface update (#1438)
    **Motivation:**
    
    We want bindings to get started on offchain. Also need to add
    `IOperatorTableCalculator` and update `ICrossChainRegistry` interface.
    
    **Modifications:**
    
    - Bindings
    - Add `IOperatorTableCalculator`
    - Add `ICrossChainRegistry`
    
    **Result:**
    
    POC ready

---
+++ src/contracts/interfaces/IOperatorTableCalculator.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import {OperatorSet} from "../libraries/OperatorSetLib.sol";
+
+/// @notice A base operator table calculator that all operator table calculators (ECDSA, BN254) must implement
+interface IOperatorTableCalculator {
+    /**
+     * @notice Calculates the operatorTableBytes for a given operatorSet
+     * @param operatorSet the operatorSet to calculate the operator table for
+     * @return operatorTableBytes the operatorTableBytes for the given operatorSet
+     */
+    function calculateOperatorTableBytes(
+        OperatorSet calldata operatorSet
+    ) external view returns (bytes memory operatorTableBytes);
+}
