4b55b583cb6dc4a7fe1baeddfc9458a27132863c
Michael Sun <michaelsun97@gmail.com>
2025-06-05 19:42:23 -0400

    fix: rebase

--- src/contracts/interfaces/IKeyRegistrar.sol
+++ src/contracts/interfaces/IKeyRegistrar.sol
@@ -13,6 +13,7 @@ interface IKeyRegistrarErrors {
     error ZeroPubkey();
     error InvalidCurveType();
     error InvalidKeypair();
+    error InvalidSignature();
     error ConfigurationAlreadySet();
     error OperatorSetNotConfigured();
     error KeyNotFound(OperatorSet operatorSet, address operator);
@@ -41,6 +42,14 @@ interface IKeyRegistrarEvents is IKeyRegistrarTypes {
 }
 
 interface IKeyRegistrar is IKeyRegistrarErrors, IKeyRegistrarEvents, ISemVerMixin {
+    /**
+     * @notice Initializes the contract ownership
+     * @param initialOwner Initial owner of the contract
+     */
+    function initialize(
+        address initialOwner
+    ) external;
+
     /**
      * @notice Configures an operator set with curve type
      * @param operatorSet The operator set to configure
