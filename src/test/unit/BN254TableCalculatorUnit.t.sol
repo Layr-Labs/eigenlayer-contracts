d327c28f846b1dc7a0a5f88f77350838c7021d5f
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-04 20:30:24 -0400

    chore: add unit test scaffolding

---
+++ src/test/unit/BN254TableCalculatorUnit.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "src/test/utils/EigenLayerMultichainUnitTestSetup.sol";
+import "src/contracts/multichain/BN254TableCalculator.sol";
+
+contract BN254TableCalculatorUnitTests is EigenLayerMultichainUnitTestSetup {
+    using StdStyle for *;
+    using ArrayLib for *;
+
+    /// @notice Pointers to the BN254TableCalculator and its implementation
+    BN254TableCalculator bn254TableCalculator;
+
+    /// @notice The default operatorSet
+    OperatorSet defaultOperatorSet = OperatorSet({avs: address(this), id: 0});
+
+    function setUp() public override {
+        EigenLayerMultichainUnitTestSetup.setUp();
+
+        // Deploy BN254TableCalculator (immutable, non-upgradeable)
+        bn254TableCalculator =
+            new BN254TableCalculator(IKeyRegistrar(address(keyRegistrar)), IAllocationManager(address(allocationManagerMock)));
+    }
+}
