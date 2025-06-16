60de59f07a965f97a46e3bbaa6a8117f1ef4b28b
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-16 16:25:59 -0400

    feat: add view function for `getGlobalConfirmerSetReferenceTimestamp` (#1471)
    **Motivation:**
    
    The `globalRootConfirmerSet` has its table only updated upon
    initialization. We should have a view function in `operatorTableUpdater`
    
    **Modifications:**
    
    Add `getGlobalConfirmerSetReferenceTimestamp`. 
    
    **Result:**
    
    Cleaner introspection

--- src/test/unit/OperatorTableUpdaterUnit.t.sol
+++ src/test/unit/OperatorTableUpdaterUnit.t.sol
@@ -178,6 +178,7 @@ contract OperatorTableUpdaterUnitTests_initialize is OperatorTableUpdaterUnitTes
         assertEq(confirmerSet.avs, address(0xDEADBEEF));
         assertEq(confirmerSet.id, 0);
         assertEq(operatorTableUpdater.getLatestReferenceTimestamp(), uint32(block.timestamp - 1));
+        assertEq(operatorTableUpdater.getGlobalConfirmerSetReferenceTimestamp(), uint32(block.timestamp - 1));
 
         uint16 threshold = operatorTableUpdater.globalRootConfirmationThreshold();
         assertEq(threshold, GLOBAL_ROOT_CONFIRMATION_THRESHOLD);
