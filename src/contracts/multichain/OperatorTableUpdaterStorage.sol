4e40c1699ed16d9b1a6257b1038b02aea2cc8d08
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-23 20:49:50 -0400

    feat: add functionality

--- src/contracts/multichain/OperatorTableUpdaterStorage.sol
+++ src/contracts/multichain/OperatorTableUpdaterStorage.sol
@@ -59,5 +59,5 @@ abstract contract OperatorTableUpdaterStorage is IOperatorTableUpdater {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[44] private __gap;
+    uint256[42] private __gap;
 }
