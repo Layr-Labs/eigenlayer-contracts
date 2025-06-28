a3dbddcf66e04fbe76bbee7c0898872c07de80a1
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-06-28 10:56:48 -0400

    docs: multichain docs (#1488)
    **Motivation:**
    
    Docs for Multichain Release
    
    **Modifications:**
    
    Add Docs For:
    - `CrossChainRegistry`
    - `OperatorTableCalculator`
    - `OperatorTableUpdater`
    - `CertificateVerifier`
    - `KeyRegistrar`
    
    - Update Interfaces to be more explicit for verification return types
    
    **Result:**
    
    Docs

--- src/contracts/permissions/KeyRegistrar.sol
+++ src/contracts/permissions/KeyRegistrar.sol
@@ -73,7 +73,7 @@ contract KeyRegistrar is KeyRegistrarStorage, PermissionControllerMixin, Signatu
         CurveType curveType = _operatorSetCurveTypes[operatorSet.key()];
         require(curveType != CurveType.NONE, OperatorSetNotConfigured());
 
-        // Check if the key is already registered
+        // Check if the operator is already registered to the operatorSet
         require(!_operatorKeyInfo[operatorSet.key()][operator].isRegistered, KeyAlreadyRegistered());
 
         // Register key based on curve type - both now require signature verification
