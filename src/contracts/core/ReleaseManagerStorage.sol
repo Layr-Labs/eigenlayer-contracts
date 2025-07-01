7d736a529a1a2a594a9a6a1933dceef0748e2feb
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-07-01 11:45:19 -0400

    feat: add `publishMetadataURI` (#1492)
    **Motivation:**
    
    *Explain here the context, and why you're making that change. What is
    the problem you're trying to solve.*
    
    **Modifications:**
    
    *Describe the modifications you've done.*
    
    **Result:**
    
    *After your change, what will change.*

--- src/contracts/core/ReleaseManagerStorage.sol
+++ src/contracts/core/ReleaseManagerStorage.sol
@@ -10,6 +10,9 @@ abstract contract ReleaseManagerStorage is IReleaseManager {
     /// @notice Returns an array of releases for a given operator set.
     mapping(bytes32 operatorSetKey => Release[]) internal _operatorSetReleases;
 
+    /// @notice Returns the metadata URI for a given operator set.
+    mapping(bytes32 operatorSetKey => string metadataURI) internal _operatorSetMetadataURI;
+
     /**
      * @dev This empty reserved space is put in place to allow future versions to add new
      * variables without shifting down storage in the inheritance chain.
--- src/contracts/interfaces/IReleaseManager.sol
+++ src/contracts/interfaces/IReleaseManager.sol
@@ -4,17 +4,23 @@ pragma solidity ^0.8.27;
 import "../libraries/OperatorSetLib.sol";
 
 interface IReleaseManagerErrors {
+    /// @notice Thrown when a metadata URI must be published before publishing a release.
+    error MustPublishMetadataURI();
+
     /// @notice Thrown when the upgrade by time is in the past.
     error InvalidUpgradeByTime();
+
+    /// @notice Thrown when the metadata URI is empty.
+    error InvalidMetadataURI();
 }
 
 interface IReleaseManagerTypes {
     /// @notice Represents a software artifact with its digest and registry URL.
     /// @param digest The hash digest of the artifact.
-    /// @param registryUrl The URL where the artifact can be found.
+    /// @param registry Where the artifact can be found.
     struct Artifact {
         bytes32 digest;
-        string registryUrl;
+        string registry;
     }
 
     /// @notice Represents a release containing multiple artifacts and an upgrade deadline.
@@ -32,6 +38,11 @@ interface IReleaseManagerEvents is IReleaseManagerTypes {
     /// @param releaseId The id of the release that was published.
     /// @param release The release that was published.
     event ReleasePublished(OperatorSet indexed operatorSet, uint256 indexed releaseId, Release release);
+
+    /// @notice Emitted when a metadata URI is published.
+    /// @param operatorSet The operator set this metadata URI is for.
+    /// @param metadataURI The metadata URI that was published.
+    event MetadataURIPublished(OperatorSet indexed operatorSet, string metadataURI);
 }
 
 interface IReleaseManager is IReleaseManagerErrors, IReleaseManagerEvents {
@@ -49,6 +60,11 @@ interface IReleaseManager is IReleaseManagerErrors, IReleaseManagerEvents {
         OperatorSet calldata operatorSet,
         Release calldata release
     ) external returns (uint256 releaseId);
+
+    /// @notice Publishes a metadata URI for an operator set.
+    /// @param operatorSet The operator set this metadata URI is for.
+    /// @param metadataURI The metadata URI that was published.
+    function publishMetadataURI(OperatorSet calldata operatorSet, string calldata metadataURI) external;
 
     /**
      *
@@ -89,4 +105,11 @@ interface IReleaseManager is IReleaseManagerErrors, IReleaseManagerEvents {
     /// @param releaseId The id of the release to check.
     /// @return True if the release is the latest release, false otherwise.
     function isValidRelease(OperatorSet memory operatorSet, uint256 releaseId) external view returns (bool);
+
+    /// @notice Returns the metadata URI for an operator set.
+    /// @param operatorSet The operator set to query.
+    /// @return The metadata URI for the operator set.
+    function getMetadataURI(
+        OperatorSet memory operatorSet
+    ) external view returns (string memory);
 }
