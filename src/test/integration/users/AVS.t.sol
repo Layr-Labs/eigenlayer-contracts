73e237ea80cffe345405d869f6b4a72bf5ee9fda
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-07-02 14:06:51 -0400

    fix: strategy manager gap (#1508)
    **Motivation:**
    
    Incorrect gap, off by one.
    
    **Modifications:**
    
    - Updated SM gap.
    
    **Result:**
    
    Correct SM gap.

--- src/test/integration/users/AVS.t.sol
+++ src/test/integration/users/AVS.t.sol
@@ -38,6 +38,7 @@ contract AVS is Logger, IAllocationManagerTypes, IAVSRegistrar {
     IStrategyManager immutable strategyManager;
     IStrategyFactory immutable strategyFactory;
     TimeMachine immutable timeMachine;
+    IKeyRegistrar immutable keyRegistrar;
     string _NAME;
 
     uint32 totalOperatorSets;
@@ -50,6 +51,7 @@ contract AVS is Logger, IAllocationManagerTypes, IAVSRegistrar {
         strategyManager = deployer.strategyManager();
         strategyFactory = deployer.strategyFactory();
         timeMachine = deployer.timeMachine();
+        keyRegistrar = deployer.keyRegistrar();
         _NAME = name;
         cheats.label(address(this), NAME_COLORED());
     }
