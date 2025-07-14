4ea9e76d4d23d1f4e0a089de7970661210fefc34
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-07-14 17:36:33 -0400

    chore: docs

--- src/contracts/multichain/ECDSACertificateVerifierStorage.sol
+++ src/contracts/multichain/ECDSACertificateVerifierStorage.sol
@@ -59,5 +59,5 @@ abstract contract ECDSACertificateVerifierStorage is IECDSACertificateVerifier {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[45] private __gap;
+    uint256[44] private __gap;
 }
