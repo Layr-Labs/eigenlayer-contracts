de4d331d07000df8e30ce790d814b7bad94a338d
Rajath Alex <rajathalex@gmail.com>
2025-07-15 17:08:26 -0400

    fix: mock certificate verifiers (#1545)
    **Motivation:**
    
    We needed to fix the build as a result of the rebase. There were some
    new functions added to the Certificate verifier interfaces which need to
    be reflected in the Mocks.
    
    **Modifications:**
    
    Updated Mock certificate verifiers
    
    **Result:**
    
    Successful build

--- src/test/mocks/MockBN254CertificateVerifier.sol
+++ src/test/mocks/MockBN254CertificateVerifier.sol
@@ -107,4 +107,8 @@ contract MockBN254CertificateVerifier is IBN254CertificateVerifier {
             totalWeights: totalWeights
         });
     }
+
+    function isReferenceTimestampSet(OperatorSet memory, uint32) external pure returns (bool) {
+        return true;
+    }
 }
