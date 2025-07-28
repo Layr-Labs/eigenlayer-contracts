358352cdd3a12d21eb84370da0ac855d7f1416a7
Nadir Akhtar <nadirakhtar123@gmail.com>
2025-07-28 16:52:44 -0700

    fix: correct gap in storage

--- src/contracts/core/ReleaseManagerStorage.sol
+++ src/contracts/core/ReleaseManagerStorage.sol
@@ -18,5 +18,5 @@ abstract contract ReleaseManagerStorage is IReleaseManager {
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[48] private __gap;
 }
