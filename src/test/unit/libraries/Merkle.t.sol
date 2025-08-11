6d3ea8451f4d50b030bdb8417049395a586658c8
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-08-11 16:46:06 -0400

    wip

---
+++ src/test/unit/libraries/Merkle.t.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.27;
+
+import "forge-std/Test.sol";
+import "src/contracts/libraries/Merkle.sol";
+import "src/test/utils/Murky.sol";
+
+abstract contract MerkleBaseTest is Test, MurkyBase {
+    bool usingSha;
+    bytes32[] leaves;
+    bytes32 root;
+    bytes[] proofs;
+
+    function test_verifyInclusion_ValidProof() public {
+        assertValidProofs();
+    }
+
+    function test_verifyInclusion_EmptyProofs() public {
+        proofs = new bytes[](proofs.length);
+        assertInvalidProofs();
+    }
+
+    function assertValidProofs() internal virtual {
+        function (bytes memory proof, bytes32 root, bytes32 leaf, uint256 index) returns (bool) verifyInclusion =
+            usingSha ? Merkle.verifyInclusionSha256 : Merkle.verifyInclusionKeccak;
+        for (uint i = 0; i < leaves.length; ++i) {
+            assertTrue(verifyInclusion(proofs[i], root, leaves[i], i), "invalid proof");
+        }
+    }
+
+    function assertInvalidProofs() internal virtual {
+        function (bytes memory proof, bytes32 root, bytes32 leaf, uint256 index) returns (bool) verifyInclusion =
+            usingSha ? Merkle.verifyInclusionSha256 : Merkle.verifyInclusionKeccak;
+        for (uint i = 0; i < leaves.length; ++i) {
+            assertFalse(verifyInclusion(proofs[i], root, leaves[i], i), "valid proof");
+        }
+    }
+
+    function getLeaves(uint numLeaves) internal view virtual returns (bytes32[] memory leaves) {
+        bytes memory _leavesAsBytes = vm.randomBytes(numLeaves * 32);
+        /// @solidity memory-safe-assembly
+        assembly {
+            leaves := _leavesAsBytes // Typecast bytes -> bytes32[].
+            mstore(leaves, numLeaves) // Update length n*32 -> n.
+        }
+    }
+
+    function getProofs(bytes32[] memory leaves) public view virtual returns (bytes[] memory proofs);
+}
+
+contract MerkleKeccakTest is MerkleBaseTest, MerkleKeccak {
+    function setUp() public {
+        usingSha = false;
+        leaves = getLeaves(vm.randomBool() ? 9 : 10);
+        root = Merkle.merkleizeKeccak(leaves);
+        proofs = getProofs(leaves);
+    }
+
+    function nextPowerOf2(uint v) internal pure returns (uint) {
+        unchecked {
+            // Round up to the next power of 2 using the method described here:
+            // https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+            if (v == 0) return 0;
+            v -= 1;
+            v |= v >> 1;
+            v |= v >> 2;
+            v |= v >> 4;
+            v |= v >> 8;
+            v |= v >> 16;
+            v |= v >> 32;
+            v |= v >> 64;
+            v |= v >> 128;
+            return v + 1;
+        }
+    }
+
+    function getProofs(bytes32[] memory leaves) public view virtual override returns (bytes[] memory proofs) {
+        // Merkle.merkleizeKeccak pads to next power of 2, so we need to match that.
+        uint numLeaves = nextPowerOf2(leaves.length);
+        bytes32[] memory paddedLeaves = new bytes32[](numLeaves);
+        for (uint i = 0; i < leaves.length; ++i) {
+            paddedLeaves[i] = leaves[i];
+        }
+
+        proofs = new bytes[](leaves.length);
+        for (uint i = 0; i < leaves.length; ++i) {
+            proofs[i] = abi.encodePacked(getProof(paddedLeaves, i));
+        }
+    }
+}
