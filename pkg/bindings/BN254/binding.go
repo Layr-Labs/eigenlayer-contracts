e391c88546f4ec8b182607c52473494a18249b76
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-09-19 09:20:00 -0400

    chore: bindings

--- pkg/bindings/BN254/binding.go
+++ pkg/bindings/BN254/binding.go
@@ -32,7 +32,7 @@ var (
 // BN254MetaData contains all meta data concerning the BN254 contract.
 var BN254MetaData = &bind.MetaData{
 	ABI: "[{\"type\":\"error\",\"name\":\"ECAddFailed\",\"inputs\":[]},{\"type\":\"error\",\"name\":\"ECMulFailed\",\"inputs\":[]},{\"type\":\"error\",\"name\":\"ECPairingFailed\",\"inputs\":[]},{\"type\":\"error\",\"name\":\"ExpModFailed\",\"inputs\":[]},{\"type\":\"error\",\"name\":\"ScalarTooLarge\",\"inputs\":[]}]",
-	Bin: "0x60556032600b8282823980515f1a607314602657634e487b7160e01b5f525f60045260245ffd5b305f52607381538281f3fe730000000000000000000000000000000000000000301460806040525f5ffdfea26469706673582212205d5cbdf978bc8397dd867db7f831e02fd1840ae85553aaf69b449a540b2ae60a64736f6c634300081b0033",
+	Bin: "0x60556032600b8282823980515f1a607314602657634e487b7160e01b5f525f60045260245ffd5b305f52607381538281f3fe730000000000000000000000000000000000000000301460806040525f5ffdfea2646970667358221220d084bedd451fadde14a6590fa5fb3e40f2250b7f49f9228d15f27cae51d34f7464736f6c634300081b0033",
 }
 
 // BN254ABI is the input ABI used to generate the binding from.
