1103809423d4190fc97a04f457e12a6544c5fa7e
Rajath Alex <rajathalex@gmail.com>
2025-11-19 12:53:31 -0500

    feat: rewards v2.2 (#1659)
    **Motivation:**
    
    We need to support Rewards v2.2 :
    - Rewards that are linear to allocated unique stake (Needs to be both
    retroactive and future-looking).
    - Rewards that are linear to total stake (Needs to be both retroactive
    and future-looking).
    
    TDD:
    https://www.notion.so/eigen-labs/PRD-Rewards-v2-2-Operator-Set-Rewards-with-Unique-Total-Stake-28513c11c3e080cbb856e41ddc6362f5
    
    **Modifications:**
    
    Updates in `RewardsCoordinator`:
    - New `createUniqueStakeRewardsSubmission` function with
    `UniqueStakeRewardsSubmissionCreated` event emission
    - New `PAUSED_UNIQUE_STAKE_REWARDS_SUBMISSION` constant
    - New `isUniqueStakeRewardsSubmissionHash` mapping
    - New `createTotalStakeRewardsSubmission` function with
    `TotalStakeRewardsSubmissionCreated` event emission
    - New `PAUSED_TOTAL_STAKE_REWARDS_SUBMISSION` constant
    - New `isTotalStakeRewardsSubmissionHash` mapping
    - Updated storage gap to 33 slots (from 35 slots)
    
    Updated Bindings
    
    **Result:**
    
    Support for Rewards v2.2

--- src/contracts/interfaces/IRewardsCoordinator.sol
+++ src/contracts/interfaces/IRewardsCoordinator.sol
@@ -331,6 +331,38 @@ interface IRewardsCoordinatorEvents is IRewardsCoordinatorTypes {
         OperatorDirectedRewardsSubmission operatorDirectedRewardsSubmission
     );
 
+    /**
+     * @notice Emitted when an AVS creates a valid `UniqueStakeRewardsSubmission` for an operator set.
+     * @param caller The address calling `createUniqueStakeRewardsSubmission`.
+     * @param rewardsSubmissionHash Keccak256 hash of (`avs`, `submissionNonce` and `rewardsSubmission`).
+     * @param operatorSet The operatorSet on behalf of which the rewards are being submitted.
+     * @param submissionNonce Current nonce of the avs. Used to generate a unique submission hash.
+     * @param rewardsSubmission The Rewards Submission. Contains the token, start timestamp, duration, strategies and multipliers.
+     */
+    event UniqueStakeRewardsSubmissionCreated(
+        address indexed caller,
+        bytes32 indexed rewardsSubmissionHash,
+        OperatorSet operatorSet,
+        uint256 submissionNonce,
+        RewardsSubmission rewardsSubmission
+    );
+
+    /**
+     * @notice Emitted when an AVS creates a valid `TotalStakeRewardsSubmission` for an operator set.
+     * @param caller The address calling `createTotalStakeRewardsSubmission`.
+     * @param rewardsSubmissionHash Keccak256 hash of (`avs`, `submissionNonce` and `rewardsSubmission`).
+     * @param operatorSet The operatorSet on behalf of which the rewards are being submitted.
+     * @param submissionNonce Current nonce of the avs. Used to generate a unique submission hash.
+     * @param rewardsSubmission The Rewards Submission. Contains the token, start timestamp, duration, strategies and multipliers.
+     */
+    event TotalStakeRewardsSubmissionCreated(
+        address indexed caller,
+        bytes32 indexed rewardsSubmissionHash,
+        OperatorSet operatorSet,
+        uint256 submissionNonce,
+        RewardsSubmission rewardsSubmission
+    );
+
     /// @notice rewardsUpdater is responsible for submitting DistributionRoots, only owner can set rewardsUpdater
     event RewardsUpdaterSet(address indexed oldRewardsUpdater, address indexed newRewardsUpdater);
 
@@ -511,6 +543,40 @@ interface IRewardsCoordinator is IRewardsCoordinatorErrors, IRewardsCoordinatorE
     function createOperatorDirectedOperatorSetRewardsSubmission(
         OperatorSet calldata operatorSet,
         OperatorDirectedRewardsSubmission[] calldata operatorDirectedRewardsSubmissions
+    ) external;
+
+    /**
+     * @notice Creates a new unique stake rewards submission for an operator set, to be split amongst the operators and
+     * set of stakers delegated to operators. The operators have to both be registered and allocate unique slashable stake to the operator set to be rewarded.
+     * @param operatorSet The operator set for which the rewards are being submitted
+     * @param rewardsSubmissions The rewards submissions being created
+     * @dev Expected to be called by the AVS that created the operator set
+     * @dev The duration of the `rewardsSubmission` cannot exceed `MAX_REWARDS_DURATION`
+     * @dev The duration of the `rewardsSubmission` cannot be 0 and must be a multiple of `CALCULATION_INTERVAL_SECONDS`
+     * @dev The tokens are sent to the `RewardsCoordinator` contract
+     * @dev Strategies must be in ascending order of addresses to check for duplicates
+     * @dev This function will revert if the `rewardsSubmissions` is malformed.
+     */
+    function createUniqueStakeRewardsSubmission(
+        OperatorSet calldata operatorSet,
+        RewardsSubmission[] calldata rewardsSubmissions
+    ) external;
+
+    /**
+     * @notice Creates a new total stake rewards submission for an operator set, to be split amongst the operators and
+     * set of stakers delegated to operators. The operators have to just be registered to the operator set to be rewarded.
+     * @param operatorSet The operator set for which the rewards are being submitted
+     * @param rewardsSubmissions The rewards submissions being created
+     * @dev Expected to be called by the AVS that created the operator set
+     * @dev The duration of the `rewardsSubmission` cannot exceed `MAX_REWARDS_DURATION`
+     * @dev The duration of the `rewardsSubmission` cannot be 0 and must be a multiple of `CALCULATION_INTERVAL_SECONDS`
+     * @dev The tokens are sent to the `RewardsCoordinator` contract
+     * @dev Strategies must be in ascending order of addresses to check for duplicates
+     * @dev This function will revert if the `rewardsSubmissions` is malformed.
+     */
+    function createTotalStakeRewardsSubmission(
+        OperatorSet calldata operatorSet,
+        RewardsSubmission[] calldata rewardsSubmissions
     ) external;
 
     /**
