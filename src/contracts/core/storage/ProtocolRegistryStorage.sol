2eaee426139c2f89817223ebcc3c914c2ab76748
Nadir Akhtar <nadir-akhtar@users.noreply.github.com>
2025-12-02 06:32:47 -0800

    fix: correct storage gap for ProtocolRegistryStorage (#1673)
    Storage gaps should add up to 50. As demonstrated by inspecting storage
    via `forge`:
    
    ```txt
    % forge inspect ProtocolRegistryStorage storage
    
    ╭--------------------+--------------------------------------------------------------------+------+--------+-------+--------------------------------------------------------------------------------╮
    | Name               | Type                                                               | Slot | Offset | Bytes | Contract                                                                       |
    +==================================================================================================================================================================================================+
    | _semanticVersion   | ShortString                                                        | 0    | 0      | 32    | src/contracts/core/storage/ProtocolRegistryStorage.sol:ProtocolRegistryStorage |
    |--------------------+--------------------------------------------------------------------+------+--------+-------+--------------------------------------------------------------------------------|
    | _deployments       | struct EnumerableMap.UintToAddressMap                              | 1    | 0      | 96    | src/contracts/core/storage/ProtocolRegistryStorage.sol:ProtocolRegistryStorage |
    |--------------------+--------------------------------------------------------------------+------+--------+-------+--------------------------------------------------------------------------------|
    | _deploymentConfigs | mapping(address => struct IProtocolRegistryTypes.DeploymentConfig) | 4    | 0      | 32    | src/contracts/core/storage/ProtocolRegistryStorage.sol:ProtocolRegistryStorage |
    |--------------------+--------------------------------------------------------------------+------+--------+-------+--------------------------------------------------------------------------------|
    | __gap              | uint256[47]                                                        | 5    | 0      | 1504  | src/contracts/core/storage/ProtocolRegistryStorage.sol:ProtocolRegistryStorage |
    ╰--------------------+--------------------------------------------------------------------+------+--------+-------+--------------------------------------------------------------------------------╯
    ```
    
    The `__gap` begins at slot `5` -- however, the gap is of size `47`,
    which adds up to `52`. Though this is not an issue, it does defy
    convention, and can cause confusion later if a developer assumes the gap
    needs to be corrected to add up to 50.
    
    ---------
    
    Co-authored-by: Nadir Akhtar <9601075+nadir-akhtar@users.noreply.github.com>
    Co-authored-by: Yash Patil <40046473+ypatil12@users.noreply.github.com>

--- src/contracts/core/storage/ProtocolRegistryStorage.sol
+++ src/contracts/core/storage/ProtocolRegistryStorage.sol
@@ -28,5 +28,5 @@ abstract contract ProtocolRegistryStorage is IProtocolRegistry {
     /// @dev This empty reserved space is put in place to allow future versions to add new
     /// variables without shifting down storage in the inheritance chain.
     /// See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-    uint256[47] private __gap;
+    uint256[45] private __gap;
 }
