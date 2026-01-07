b0a9d70ff9eb1e9dd7ef16b0163ee5317d193be6
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2026-01-07 17:01:43 -0500

    gap

--- src/contracts/core/storage/EmissionsControllerStorage.sol
+++ src/contracts/core/storage/EmissionsControllerStorage.sol
@@ -63,5 +63,5 @@ abstract contract EmissionsControllerStorage is IEmissionsController {
     /// @dev This empty reserved space is put in place to allow future versions to add new
     /// variables without shifting down storage in the inheritance chain.
     /// See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
-    uint256[45] private __gap;
+    uint256[46] private __gap;
 }
